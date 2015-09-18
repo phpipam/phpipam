2050eeabb6da0c82bd72b81d9f9110c0c8d4e0d0
Sneddo <github@sneddo.net>
2015-09-18 10:18:03 +1000

    Update version check to include footer if check fails

--- app/admin/version-check/index.php
+++ app/admin/version-check/index.php
@@ -5,15 +5,15 @@
 
 # verify that user is logged in
 $User->check_user_session();
-
+echo $User->settings->version
 # get latest version */
-if(!$version = $Tools->check_latest_phpipam_version()) { $Result->show("danger", _("Version check failed").'!', true); }
+if(!$version = $Tools->check_latest_phpipam_version()) 	{ $Result->show("danger", _("Version check failed").'!', false); }
+else {
+	print "<h4>phpIPAM version check</h4><hr>";
 
-
-print "<h4>phpIPAM version check</h4><hr>";
-
-//print result
-if($User->settings->version == $version) 		{ $Result->show("success alert-absolute", _('Latest version').' ('. $User->settings->version .') '._('already installed').'!', false); }
-else if ($User->settings->version > $version) 	{ $Result->show("success alert-absolute", _('Development version').' ('. $User->settings->version .') '._('installed! Latest production version is').' '. $version, false);}
-else 											{ $Result->show("danger alert-absolute",  _('New version of phpipam available').':</b><hr>'._('Installed version').': '.$User->settings->version."<br>"._('Available version').': '. $version."<br><br>"._('You can download new version').' <a href="https://sourceforge.net/projects/phpipam/files/current/phpipam-'. $version .'.tar/download">'._('here').'</a>.', false); }
+	//print result
+	if($User->settings->version == $version) 		{ $Result->show("success alert-absolute", _('Latest version').' ('. $User->settings->version .') '._('already installed').'!', false); }
+	else if ($User->settings->version > $version) 	{ $Result->show("success alert-absolute", _('Development version').' ('. $User->settings->version .') '._('installed! Latest production version is').' '. $version, false);}
+	else 											{ $Result->show("danger alert-absolute",  _('New version of phpipam available').':</b><hr>'._('Installed version').': '.$User->settings->version."<br>"._('Available version').': '. $version."<br><br>"._('You can download new version').' <a href="https://sourceforge.net/projects/phpipam/files/current/phpipam-'. $version .'.tar/download">'._('here').'</a>.', false); }
+}
 ?>
