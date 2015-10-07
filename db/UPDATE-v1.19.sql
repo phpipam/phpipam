15c8ffda36a8e6d3d7f1536aa3bff3f927ff70fe
fxed <git@feph.de>
2015-10-07 09:21:09 +0200

    updated schema and 1.19 sql
    added the neccessary tables and fields for the firewall zone management
    module.

--- db/UPDATE-v1.19.sql
+++ db/UPDATE-v1.19.sql
@@ -32,3 +32,36 @@ ALTER TABLE `ipaddresses` ADD UNIQUE INDEX `sid_ip_unique` (`ip_addr`, `subnetId
 
 /* add tag to ip requests */
 ALTER TABLE `requests` ADD `state` INT  NULL  DEFAULT '2'  AFTER `dns_name`;
+
+/* The firewall zones table holds the information of the zones. */
+CREATE TABLE `firewallZones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `generator` tinyint(1) NOT NULL,
+  `length` int(2) DEFAULT NULL,
+  `padding` tinyint(1) DEFAULT NULL,
+  `zone` varchar(31) COLLATE utf8_unicode_ci NOT NULL,
+  `indicator` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `subnetId` int(11) unsigned DEFAULT NULL,
+  `stacked` varchar(1024) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `vlanId` int(11) unsigned DEFAULT NULL,
+  `permissions` varchar(1024) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `editDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/* Firewall zone mapping table holds the information of device is part of a zone, plus some extra informations */
+CREATE TABLE `firewallZoneMapping` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `zoneId` int(11) unsigned NOT NULL,
+  `alias` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `deviceId` int(11) unsigned DEFAULT NULL,
+  `interface` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `editDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/* Alter the settings table to inject the modul switch and default zone settings */
+ALTER TABLE `settings` 
+ADD COLUMN `enableFirewallZones` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '' AFTER `enableDNSresolving`,
+ADD COLUMN `firewallZoneSettings` VARCHAR(1024) NOT NULL DEFAULT '{"zoneLength":3,"ipType":{"0":"v4","1":"v6"},"separator":"_","indicator":{"0":"own","1":"customer"},"zoneGenerator":"2","zoneGeneratorType":{"0":"decimal","1":"hex","2":"text"},"deviceType":"3","padding":"on","strictMode":"on"}' COMMENT '' AFTER `enableFirewallZones`;
