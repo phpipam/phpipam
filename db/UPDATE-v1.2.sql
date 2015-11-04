c6bae2f439e681b1e938a59031b92ebe71ec4561
phpipam.net <root@phpipam.net>
2015-11-04 20:50:15 +0100

    Fixed update queries and error exits

--- db/UPDATE-v1.2.sql
+++ db/UPDATE-v1.2.sql
@@ -2,8 +2,7 @@
 UPDATE `settings` set `version` = '1.2';
 
 /* reset db check field and donation */
-UPDATE `settings` set `dbverified` = 0;
-UPDATE `settings` set `donate` = 0;
+UPDATE `settings` set `dbverified` = 0, `donate` = 0;
 
 /* add subnetView Setting */
 ALTER TABLE `settings` ADD `subnetView` TINYINT  NOT NULL  DEFAULT '0';
