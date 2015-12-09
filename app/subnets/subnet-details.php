13072cbc259ba232fe3d8e1c77a02a44fbd740d8
Davide Cecchetto <dcecchetto@gmail.com>
2015-12-09 12:05:37 +0100

    Update subnet-details.php

--- app/subnets/subnet-details.php
+++ app/subnets/subnet-details.php
@@ -476,7 +476,7 @@ $rowSpan = 10 + sizeof($custom_fields);
 		else
 		print "<a class='modIPaddr btn btn-xs btn-default btn-success' 	href='' data-container='body' rel='tooltip' title='"._('Add new IP address')."' data-subnetId='$subnet[id]' data-action='add' data-id=''>	<i class='fa fa-plus'></i></a> ";
 		//requests
-		if($subnet['allowRequests'] == 1  && $subnet_permission<3)  {
+		if($User->settings->enableIPrequests==1 && $subnet['allowRequests'] == 1  && $subnet_permission<3)  {
 		print "<a class='request_ipaddress btn btn-xs btn-default btn-success' 	href='' data-container='body' rel='tooltip' title='"._('Request new IP address')."' data-subnetId='$subnet[id]'>					<i class='fa fa-plus-circle'>  </i></a>";
 		}
 		// subnet scan
