b231e2708b7d2df2f605032859f059a478d3b09d
phpipam.net <root@phpipam.net>
2016-01-28 14:59:44 +0100

    Multiple master servers #320

--- app/admin/powerDNS/domain-edit-result.php
+++ app/admin/powerDNS/domain-edit-result.php
@@ -28,7 +28,17 @@ if ($_POST['action']!="delete") {
 	if($Result->validate_hostname($_POST['name'])===false)			{ $Result->show("danger", "Invalid domain name", true); }
 	// master
 	if (strlen($_POST['master'])>0) {
-		if(!filter_var($_POST['master'], FILTER_VALIDATE_IP))		{ $Result->show("danger", "Master must be an IP address", true); }
+    	// if multilpe masters
+    	if (strpos($_POST['master'], ";")!==false) {
+        	// to array and trim, check each
+        	$masters = array_filter(explode(";", $_POST['master']));
+        	foreach ($masters as $m) {
+                           if(!filter_var($m, FILTER_VALIDATE_IP))  { $Result->show("danger", "Master must be an IP address". " - ". $m, true); }
+        	}
+    	}
+    	else {
+            if(!filter_var($_POST['master'], FILTER_VALIDATE_IP))	{ $Result->show("danger", "Master must be an IP address". " - ". $_POST['master'], true); }
+    	}
 	}
 	// type
 	if(!in_array($_POST['type'], (array )$PowerDNS->domain_types))	{ $Result->show("danger", "Invalid domain type", true); }
@@ -37,6 +47,23 @@ if ($_POST['action']!="delete") {
 	if ($_POST['action']=="add" && !isset($_POST['manual'])) {
 		// admin
 		if ($Result->validate_email($_POST['hostmaster'])===false)	{ $Result->show("danger", "Invalid domain admin", true); }
+	}
+
+	// if slave master must be present
+	if ($_POST['type']=="SLAVE") {
+    	if (strlen($_POST['master'])==0)                            { $Result->show("danger", "Please set master server(s) if domain type is SLAVE", true); }
+        else {
+        	if (strpos($_POST['master'], ";")!==false) {
+            	// to array and trim, check each
+            	$masters = array_filter(explode(";", $_POST['master']));
+            	foreach ($masters as $m) {
+                               if(!filter_var($m, FILTER_VALIDATE_IP))  { $Result->show("danger", "Master must be an IP address". " - ". $m, true); }
+            	}
+        	}
+        	else {
+                if(!filter_var($_POST['master'], FILTER_VALIDATE_IP))	{ $Result->show("danger", "Master must be an IP address". " - ". $_POST['master'], true); }
+        	}
+    	}
 	}
 
 
