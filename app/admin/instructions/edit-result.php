7cf23ea617cd4ec9655d9b62c98ae69dde9e29cb
phpipam <root@ipam.voljatel.local>
2016-04-26 11:18:01 +0200

    Added IP request instructions

--- app/admin/instructions/edit-result.php
+++ app/admin/instructions/edit-result.php
@@ -20,17 +20,34 @@ $User->check_user_session();
 # validate csrf cookie
 $User->csrf_cookie ("validate", "instructions", $_POST['csrf_cookie']) === false ? $Result->show("danger", _("Invalid CSRF cookie"), true) : "";
 
-# execute
-#
-#	we do it directly because we permit html tags for instructions
-#
-try { $Database->updateObject("instructions", array("id"=>1, "instructions"=>$_POST['instructions']), "id"); }
-catch (Exception $e) {
-	$Result->show("danger", _("Error: ").$e->getMessage(), false);
-	$Log->write( "Instructions updated", "Failed to update instructions<hr>".$e->getMessage(), 1);
+# validate ID
+if ($_POST['id']=="1" || $_POST['id']=="2") {
+    // update
+    if($Database->objectExists("instructions", $_POST['id'])) {
+        print "update";
+        try { $Database->updateObject("instructions", array("id"=>$_POST['id'], "instructions"=>$_POST['instructions']), "id"); }
+        catch (Exception $e) {
+        	$Result->show("danger", _("Error: ").$e->getMessage(), false);
+        	$Log->write( "Instructions updated", "Failed to update instructions<hr>".$e->getMessage(), 1);
+        }
+     }
+    // create new
+    else {
+        try { $Database->insertObject("instructions", array("id"=>$_POST['id'], "instructions"=>$_POST['instructions']), false, true, false); }
+        catch (Exception $e) {
+        	$Result->show("danger", _("Error: ").$e->getMessage(), false);
+        	$Log->write( "Instructions updated", "Failed to update instructions<hr>".$e->getMessage(), 1);
+        }
+    }
+    // success
+    if (!isset($e)) {
+        # ok
+        $Log->write( "Instructions updated", "Instructions updated succesfully", 0);
+        $Result->show("success", _("Instructions updated successfully"), true);
+    }
 }
-# ok
-$Log->write( "Instructions updated", "Instructions updated succesfully", 0);
-$Result->show("success", _("Instructions updated successfully"), true);
+else {
+    $Result->show("danger", _("Invalid ID"), false);
+}
 
 ?>
