21896c66f9151c2ab2f72ccd71df82e3bfca8bd6
Miha Petkovsek <miha.petkovsek@gmail.com>
2016-05-16 21:57:14 +0200

    Fixed issue #499 - Invalid CSRF cookie when deleting device from rack

--- app/tools/racks/index.php
+++ app/tools/racks/index.php
@@ -15,7 +15,7 @@ $hidden_custom_fields = json_decode($User->settings->hiddenCustomFields, true);
 $hidden_custom_fields = is_array(@$hidden_custom_fields['racks']) ? $hidden_custom_fields['racks'] : array();
 
 # create csrf token
-$csrf = $User->csrf_cookie ("create", "racks");
+$csrf = $User->csrf_cookie ("create", "rack_devices");
 
 # all racks or one ?
 if (isset($_GET['subnetId']))   { include("print-single-rack.php"); }
