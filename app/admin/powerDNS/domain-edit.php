01fa89ca344e99f03c5202253add26e7c8218d3a
Miha Petkovsek <miha.petkovsek@gmail.com>
2016-08-24 13:00:15 +0200

    Added expire to PowerDNS creation #655

--- app/admin/powerDNS/domain-edit.php
+++ app/admin/powerDNS/domain-edit.php
@@ -181,6 +181,26 @@ $readonly = $_POST['action']=="delete" ? "readonly" : "";
 		</select>
 		</td>
 	</tr>
+
+    <!-- expire -->
+    <tr>
+            <td><?php print _('Expire'); ?></th>
+            <td>
+            <select name="expire" class="form-control input-w-auto input-sm" <?php print $readonly; ?>>
+            <?php
+            // loop
+            foreach($PowerDNS->ttl as $k=>$ttl) {
+                    // active
+                    if ($k == @$pdns->expire)       { $selected = "selected"; }
+                    else                                                    { $selected = ""; }
+                    // print
+                    print "<option value='$k' $selected>$ttl ($k)</option>";
+            }
+            ?>
+            </select>
+            </td>
+    </tr>
+
 	</tbody>
 	<!-- records -->
 	<tr>
