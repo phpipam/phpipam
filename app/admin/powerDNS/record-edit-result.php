fb5be2b51699a7501a7324ad1f702fb4ab516561
Miha Petkovsek <miha.petkovsek@gmail.com>
2016-09-02 10:04:41 +0200

    Fix for Editing PowerDNS records #726

--- app/admin/powerDNS/record-edit-result.php
+++ app/admin/powerDNS/record-edit-result.php
@@ -37,7 +37,7 @@ if ($_POST['action'] != "delete") {
 
 # validate and set values
 if ($_POST['action'] == "edit") {
-    $values = $PowerDNS->formulate_update_record($_POST['domain_id'], $_POST['name'], $_POST['type'], $_POST['content'], $_POST['ttl'], $_POST['prio'], $_POST['disabled'], $record->change_date);
+    $values = $PowerDNS->formulate_update_record($_POST['name'], $_POST['type'], $_POST['content'], $_POST['ttl'], $_POST['prio'], $_POST['disabled'], $record->change_date);
     $values['domain_id'] = $_POST['domain_id'];
 } elseif ($_POST['action'] == "add") {
     $values = $PowerDNS->formulate_new_record($_POST['domain_id'], $_POST['name'], $_POST['type'], $_POST['content'], $_POST['ttl'], $_POST['prio'], $_POST['disabled']);
