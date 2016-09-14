b5efe603c7fbb6d8c7b8903f7dc1a38e69c2d9d9
Miha Petkovsek <miha.petkovsek@gmail.com>
2016-09-14 18:31:09 +0200

    Fixed widgets local file inclusion #738

--- app/dashboard/widgets/index.php
+++ app/dashboard/widgets/index.php
@@ -2,9 +2,18 @@
 
 # get filelist for all configured widgets
 
-
-# include requested widget file
-if(!file_exists(dirname(__FILE__)."/".$_GET['section'].".php"))		{ $_REQUEST['section']="404"; print "<div id='error'>"; include_once('app/error.php'); print "</div>"; }
-else																{ include(dirname(__FILE__) . "/".$_GET['section'].".php"); }
+# validate $_POST[section]
+if(preg_match('/^[a-zA-Z0-9-_]+$/', $_GET['section'])==false) {
+    $_REQUEST['section']="404"; print "<div id='error'>"; include_once('app/error.php'); print "</div>";
+}
+# validate object
+elseif($Tools->fetch_object("widgets", "wfile", $_GET['section'])===false) {
+    $_REQUEST['section']="404"; print "<div id='error'>"; include_once('app/error.php'); print "</div>";
+}
+else {
+    # include requested widget file
+    if(!file_exists(dirname(__FILE__)."/".$_GET['section'].".php"))		{ $_REQUEST['section']="404"; print "<div id='error'>"; include_once('app/error.php'); print "</div>"; }
+    else																{ include(dirname(__FILE__) . "/".$_GET['section'].".php"); }
+}
 
 ?>
