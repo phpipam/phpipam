4382fc63c56d8a02e2846d8e993d03f0d85f21b2
Ferbla <Ferbla@users.noreply.github.com>
2016-10-06 21:27:41 -0500

    Might have fixed #788

--- app/admin/groups/edit-group.php
+++ app/admin/groups/edit-group.php
@@ -26,7 +26,7 @@ $Admin->validate_action ($_POST['action']);
 $_POST = $User->strip_input_tags ($_POST);
 
 # ID
-if(!is_numeric($_POST['id']))   $Result->show("danger", _("Invalid Id"), true, true);
+if(!is_numeric($User->user->id))   $Result->show("danger", _("Invalid Id"), true, true);
 
 # fetch group and set title
 if($_POST['action']=="add") {
