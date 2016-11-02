39739cf43945dc64fa12ddf784a486a861ba7b13
sebdew <sebastien.dewarlez@gmail.com>
2016-11-02 17:09:27 +0100

    Update Subnets.php
    Add possibility to give an id to the function read_subnet_gateway()

--- api/controllers/Subnets.php
+++ api/controllers/Subnets.php
@@ -785,8 +785,12 @@ class Subnets_controller extends Common_api_functions {
 	 * @access private
 	 * @return int|bool
 	 */
-	private function read_subnet_gateway () {
-    	return $this->Subnets->find_gateway ($this->_params->id);
+	private function read_subnet_gateway ($id=NULL) {
+    		if($id === NULL) {
+    			return $this->Subnets->find_gateway ($this->_params->id);
+		} else {
+			return $this->Subnets->find_gateway ($id);
+		}
 	}
 
 	/**
