08d2d4df6c5d6acfb13813b3e6fbb01b866b17b1
Miha Petkovsek <miha.petkovsek@gmail.com>
2017-01-13 22:04:25 +0100

    Fixed Verify database -> Invalid addresses #876

--- app/admin/verify-database/index.php
+++ app/admin/verify-database/index.php
@@ -92,7 +92,7 @@ else {
 print "<h4>"._('Invalid addresses').'</h4><hr>';
 
 $invalid_subnets = $Addresses->find_invalid_addresses();
-if ($invalid_subnets===false) {
+if ($invalid_subnets===false || sizeof($invalid_subnets)==0) {
 	$Result->show ("success", _("No invalid addresses detected"), false);
 }
 else {
