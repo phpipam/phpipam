ab66d2b8b11aa522d84904206fee123b85f5a54a
Miha Petkovsek <miha.petkovsek@gmail.com>
2017-03-08 10:58:23 +0100

    Escaped apostrophe in note bubble

--- app/subnets/addresses/print-address-table.php
+++ app/subnets/addresses/print-address-table.php
@@ -408,7 +408,10 @@ else {
 
 	       		# print info button for hover
 	       		if(in_array('note', $selected_ip_fields)) {
-	        		if(!empty($addresses[$n]->note)) 					{ print "<td class='narrow'><i class='fa fa-gray fa-comment-o' rel='tooltip' data-container='body' data-html='true' title='".str_replace("\n", "<br>",$addresses[$n]->note)."'></td>"; }
+
+	       			$addresses[$n]->note = str_replace("'", "&#39;", $addresses[$n]->note);
+
+	        		if(!empty($addresses[$n]->note)) 					{ print "<td class='narrow'><i class='fa fa-gray fa-comment-o' rel='tooltip' data-container='body' data-html='false' title='".str_replace("\n", "<br>",addslashes($addresses[$n]->note))."'></td>"; }
 	        		else 												{ print "<td class='narrow'></td>"; }
 	        	}
 
