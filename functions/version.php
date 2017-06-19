946240ec7822393c564350338a001611d2bc0ba5
Miha Petkovsek <miha.petkovsek@gmail.com>
2017-06-19 10:51:16 +0200

    Update fr_FR translation to 1.3

--- functions/version.php
+++ functions/version.php
@@ -2,9 +2,8 @@
 /* set latest version */
 define("VERSION", "1.3");									//version changes if database structure changes
 /* set latest revision */
-define("REVISION", "000");									//revision always changes, verision only if database structure changes
+define("REVISION", "001");									//revision always changes, verision only if database structure changes
 /* set last possible upgrade */
 define("LAST_POSSIBLE", "1.1");								//minimum required version to be able to upgrade
 /* prefix for css/js */
 define("SCRIPT_PREFIX", "1.3");								//css and js folder prefix to prevent caching
-?>
