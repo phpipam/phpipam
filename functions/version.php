2d2c007ff070f6d9e2b97c5f700280c41725fa0e
Miha Petkovsek <miha.petkovsek@gmail.com>
2017-11-07 08:56:30 +0100

    Renamed JS ans CSS folders to 1.3.1

--- functions/version.php
+++ functions/version.php
@@ -8,4 +8,4 @@ define("REVISION", "000");									//revision always changes, verision only if d
 /* set last possible upgrade */
 define("LAST_POSSIBLE", "1.1");								//minimum required version to be able to upgrade
 /* prefix for css/js */
-define("SCRIPT_PREFIX", "1.3");								//css and js folder prefix to prevent caching
+define("SCRIPT_PREFIX", "1.3.1");							//css and js folder prefix to prevent caching
