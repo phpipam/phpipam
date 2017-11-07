0bac55cadb1960566e8d2fdbabd7220dd63824a9
Miha Petkovsek <miha.petkovsek@gmail.com>
2017-11-07 18:19:41 +0100

    Disabled debugging on subnet export #1522

--- app/subnets/addresses/export-subnet.php
+++ app/subnets/addresses/export-subnet.php
@@ -20,8 +20,8 @@ $Addresses	= new Addresses ($Database);
 $User->check_user_session();
 
 # we dont need any errors!
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
 
 # fetch subnet details
