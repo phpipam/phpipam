9d9445ec2203b4a5b69a7902a952e1bfe33630a0
Gary Allan <github@gallan.co.uk>
2018-01-31 13:22:44 +0000

    Removal of duplicated code

--- app/tools/subnets/index.php
+++ app/tools/subnets/index.php
@@ -1,23 +1,2 @@
 <?php
-
-/**
- * Script to print subnets
- ***************************/
-
-# verify that user is logged in
-$User->check_user_session();
-
-# fetch all sections
-$sections = $Sections->fetch_all_sections();
-
-# print all sections with delete / edit button
-print '<h4>'._('Subnet management').'</h4>';
-print "<hr>";
-
-/* Foreach section fetch subnets and print it! */
-if(is_array($sections)) {
-	foreach($sections as $section) {
-		print "<br><br><h4>"._('Available subnets in section')." $section->name: [$section->description]</h4>";
-		print $Sections->print_section_subnets_table($User, $section->id);
-	}
-}
+require( dirname(__FILE__) . '/../../admin/subnets/index.php');
