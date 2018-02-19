82c22cd778d7b0357fd4d929e754b3d2c8835dee
Miha Petkovsek <miha.petkovsek@gmail.com>
2018-02-19 10:07:59 +0100

    All sections UI fix

--- app/sections/all-sections.php
+++ app/sections/all-sections.php
@@ -35,7 +35,7 @@ if ($sections !== false) {
 
 <!-- show sections -->
 <?php if($sections!==false) { ?>
-<table class="table sorted table-striped table-condensed table-top">
+<table class="table sorted table-striped table-condensed table-top" data-cookie-id-table="all_sections">
 <!-- headers -->
 <thead>
 <tr>
