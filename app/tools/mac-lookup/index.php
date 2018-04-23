1ee6e6fbffb554799837fba197aae04ac8130493
Gary Allan <github@gallan.co.uk>
2018-04-23 17:09:15 +0100

    Bugfix: Reflected xss on 'mac' parameter. #1903

--- app/tools/mac-lookup/index.php
+++ app/tools/mac-lookup/index.php
@@ -12,7 +12,7 @@ $User->check_user_session();
 <form id="mac_lookup" name="search" class='form-inline' role="form" style="margin-bottom:20px;">
 	<div class='input-group'>
 	<div class='form-group'>
-		<input class="search input-md form-control" name="mac" placeholder="<?php print _('MAC address'); ?>" value='<?php print @$_GET['mac']; ?>' type="text" autofocus="autofocus" style='width:250px;'>
+		<input class="search input-md form-control" name="mac" placeholder="<?php print _('MAC address'); ?>" value='<?php print @escape_input($_GET['mac']); ?>' type="text" autofocus="autofocus" style='width:250px;'>
 		<span class="input-group-btn">
 			<button type="submit" class="btn btn-md btn-default"><?php print _('search');?></button>
 		</span>
