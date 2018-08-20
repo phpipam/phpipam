c082a0317b75041d0457588d84ffbe7f9b325ef2
Miha Petkovsek <miha.petkovsek@gmail.com>
2018-08-20 13:57:20 +0200

    Update dbversion to 5

--- functions/version.php
+++ functions/version.php
@@ -6,7 +6,7 @@ define("VERSION_VISIBLE", "1.4");							//visible version in footer
 /* set latest revision */
 define("REVISION", "016");									//revision always changes, verision only if database structure changes
 /* set database schema version */
-define("DBVERSION", "4");									//database schema version (future feature)
+define("DBVERSION", "5");									//database schema version (future feature)
 /* set last possible upgrade */
 define("LAST_POSSIBLE", "1.1");								//minimum required version to be able to upgrade
 /* prefix for css/js */
