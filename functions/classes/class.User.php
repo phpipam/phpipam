782e881af3e8a3509abad8ba68253ea417890dcf
Michael Baker <michael.baker@bulletproof.net>
2018-08-31 13:56:27 +0800

    Fix SAML mapping

--- functions/classes/class.User.php
+++ functions/classes/class.User.php
@@ -795,7 +795,7 @@ class User extends Common_functions {
      * @return void
      */
     public function authenticate ($username, $password, $saml = false) {
-        if(($saml !== false ) && (defined('MAP_SAML_USER'))) {
+        if(($saml !== false ) && (defined('MAP_SAML_USER')) && (MAP_SAML_USER !== false)) {
             $username = SAML_USERNAME;
         }
         # first we need to check if username exists
