9a806373bd08513affc929206eb625f59cf6c4b5
Gary Allan <github@gallan.co.uk>
2018-09-04 20:03:17 +0100

    Bugfix: Check for compatible 32 bit long2ip() (broken in PHP 7.1.0)

--- functions/checks/check_php_build.php
+++ functions/checks/check_php_build.php
@@ -51,7 +51,8 @@ if (!function_exists('ctype_alnum')) {
 }
 
 # if any extension is missing print error and die!
-if (sizeof($missingExt) != 1 || (phpversion() < "5.4" && $allow_older_version!==true)) {
+if (sizeof($missingExt) != 1 || (phpversion() < "5.4" && $allow_older_version!==true) ||
+    (PHP_INT_SIZE==4 && (@long2ip('3355443200') !== '200.0.0.0'))) {
 
     /* remove dummy 0 line */
     unset($missingExt[0]);
@@ -81,11 +82,16 @@ if (sizeof($missingExt) != 1 || (phpversion() < "5.4" && $allow_older_version!==
         $error  .= _('Please recompile PHP to include missing extensions and restart Apache.') . "\n";
     }
     /* php version error */
-    else {
+    elseif(phpversion() < "5.4") {
         $error  .= "<div class='alert alert-danger' style='margin:auto;margin-top:20px;width:500px;'><strong>"._('Unsupported PHP version')."!</strong><br><hr>";
         $error  .= _('From release 1.3.2 on at least PHP version 5.4 is required!')."<br>"._('You can override this by setting $allow_older_version=true in config.php.')."<br>";
         $error  .= _("Detected PHP version: ").phpversion(). "<br><br>\n";
-        $error  .= _("Last development version can be downloaded ")." <a href='https://github.com/phpipam/phpipam/tree/9ca731d475d5830ca421bac12da31d5023f02636' target='_blank'>here</a>.";
+        $error  .= _("Last development version can be downloaded ")." <a href='https://github.com/phpipam/phpipam' target='_blank'>here</a>.";
+    }
+    else {
+        $error  .= "<div class='alert alert-danger' style='margin:auto;margin-top:20px;width:500px;'><strong>"._('Not 64-bit system')."!</strong><br><hr>";
+        $error  .= _('From PHP 7.1.0 long2ip() no longer accepts strings and is incompatible.')."<br>"._('64 bit PHP is required!')."<br>";
+        $error  .= _("Last development version can be downloaded ")." <a href='https://github.com/phpipam/phpipam' target='_blank'>here</a>.";
     }
 
     $error  .= "</body>";
