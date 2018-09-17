1a735cdbffc91b4541c1c7d4c863ba7bada4e8db
Miha Petkovsek <miha.petkovsek@gmail.com>
2018-09-17 17:09:18 +0200

    Fixed #2209

--- app/admin/users/print_module_permissions.php
+++ app/admin/users/print_module_permissions.php
@@ -13,26 +13,6 @@ if($user['role']=="Administrator") {
 }
 
 print "<table class='table-noborder popover_table'>";
-// pdns
-if ($User->settings->enablePowerDNS==1) {
-    if(strlen($user['pdns'])==0) $user['pdns'] = "No";
-    $user['pdns'] = $user['pdns']=="No" ? "<span class='badge badge1 badge5 alert-danger'>"._($user['pdns'])."</span>" : "<span class='badge badge1 badge5 alert-success'>"._($user['pdns'])."</span>";
-    print "<tr><td>"._("PowerDNS")."</td><td>".$user['pdns']."</td></tr>";
-}
-
-// vlan / VRF
-if(strlen($user['editVlan'])==0) $user['editVlan'] = "No";
-$user['editVlan'] = $user['editVlan']=="No" ? "<span class='badge badge1 badge5 alert-danger'>"._($user['editVlan'])."</span>" : "<span class='badge badge1 badge5 alert-success'>"._($user['editVlan'])."</span>";
-print "<tr><td>"._("VLANs / VRFs")."</td><td>".$user['editVlan']."</td></tr>";
-
-// Circuits
-if ($User->settings->enableCircuits==1) {
-    $user['editCircuits'] = $user['editCircuits']=="No"||is_null($user['editCircuits']) ? "<span class='badge badge1 badge5 alert-danger'>"._("No")."</span>" : "<span class='badge badge1 badge5 alert-success'>"._($user['editCircuits'])."</span>";
-    print "<tr><td>"._("Circuits")."</td><td>".$user['editCircuits']."</td></tr>";
-}
-
-
-
 
 
 // VLAN
@@ -68,16 +48,18 @@ print "<tr><td>"._("PSTN")."</td><td>".get_badge($user['perm_pstn'])."</td></tr>
 print "</table>";
 
 
-/**
- * Print permission badge
- * @method get_badge
- * @param  int $level
- * @return string
- */
-function get_badge ($level) {
-    global $Subnets;
-    // null level
-    if(is_null($level)) $level = 0;
-    // return
-    return $level=="0" ? "<span class='badge badge1 badge5 alert-danger'>"._($Subnets->parse_permissions ($level))."</span>" : "<span class='badge badge1 badge5 alert-success'>"._($Subnets->parse_permissions ($level))."</span>";
+if(!function_exists('get_badge')) {
+    /**
+     * Print permission badge
+     * @method get_badge
+     * @param  int $level
+     * @return string
+     */
+    function get_badge ($level) {
+        global $Subnets;
+        // null level
+        if(is_null($level)) $level = 0;
+        // return
+        return $level=="0" ? "<span class='badge badge1 badge5 alert-danger'>"._($Subnets->parse_permissions ($level))."</span>" : "<span class='badge badge1 badge5 alert-success'>"._($Subnets->parse_permissions ($level))."</span>";
+    }
 }
