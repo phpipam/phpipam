1d96a5d6eaabfc0a43f5209a7fed674bb2e390de
Gary Allan <github@gallan.co.uk>
2018-12-03 21:25:36 +0000

    Bugfix: Add CSRF check to user-menu widgets. #2326

--- app/tools/user-menu/user-widgets-set.php
+++ app/tools/user-menu/user-widgets-set.php
@@ -17,6 +17,9 @@ $User		= new User ($Database);
 # verify that user is logged in
 $User->check_user_session();
 
+# validate csrf cookie
+$User->Crypto->csrf_cookie ("validate", "user-menu", $_POST['csrf_cookie']) === false ? $Result->show("danger", _("Invalid CSRF cookie"), true) : "";
+
 /* save widgets */
 if (!$User->self_update_widgets ($_POST['widgets'])) 	{ $Result->show("danger", _('Error updating'),true); }
 else 													{ $Result->show("success", _('Widgets updated'),true); }
