c1f279b8ea6ece89cdbde6dbf6875c5d9ae8b881
Gary Allan <github@gallan.co.uk>
2018-12-30 17:38:28 +0000

    Bugfix: class.Session_DB::_read() retrieves incorrect session data.
    Issue:
      Session_DB::_read() calls Database::getObject("php_sessions", $id) with
      non numeric session id values. e.g. id='n7vce1ksn4t9gnljabqmt88ab6'.
    
      getObject() internally sets $id = intval($id);, we end up querying for...
      "SELECT * FROM `php_sessions` WHERE `id`=0 LIMIT 1;"
    
      This query selects the whole table and returns the first row found!
    
    Fix:
      Use findObject() to search for varchar(32) session id.

--- functions/classes/class.Session_DB.php
+++ functions/classes/class.Session_DB.php
@@ -81,9 +81,13 @@ class Session_DB {
 	public function _read ($id) {
 		// check database for cookie
 		try {
-			$session = $this->Database->getObject ("php_sessions", $id);
+			// Note: Database->getObject() does not support non-numeric id's. Use findObject().
+			$session = $this->Database->findObject ('php_sessions', 'id', $id);
 			// check
-			return $session===null ? "" : $session->data;
+			if (!is_object($session) || empty($session->data))
+				return "";
+
+			return $session->data;
 		}
 		catch (Exception $e) {
 			$this->Result->show("danger", $e->getMessage(), false);
