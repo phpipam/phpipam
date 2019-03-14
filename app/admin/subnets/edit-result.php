019908fe154c241250ce53c2165c998bb5fc061d
lrdSheriff <sh_77@mail.ru>
2019-03-14 10:16:48 +0400

    Update edit-result.php

--- app/admin/subnets/edit-result.php
+++ app/admin/subnets/edit-result.php
@@ -86,7 +86,7 @@ if ($_POST['action']=="add") {
 	# Generic checks
 
     // ID must be numberic value
-	if(!is_numeric($_POST['sectionId']))									{ $Result->show("danger", _("Invalid ID"), true); }
+	if(!is_numeric($_POST['sectionId'])) { $Result->show("danger", _("Invalid ID"), true); }
     // verify (version 2) that user has permissions to add subnet
     if($Sections->check_permission ($User->user, $_POST['sectionId']) != 3) {
         if($Subnets->check_permission ($User->user, $_POST['subnetId']) != 3) { $Result->show("danger", _('You do not have permissions to add new subnet in this section')."!", true); }
