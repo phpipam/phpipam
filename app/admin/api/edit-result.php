453f7b7a2b1adedb83f8dfd22b08e5ab30a95ab8
Gary Allan <github@gallan.co.uk>
2019-04-06 23:04:12 +0100

    Bugfix: escape $ character

--- app/admin/api/edit-result.php
+++ app/admin/api/edit-result.php
@@ -44,7 +44,7 @@ if($_POST['action']!="delete") {
     	elseif ($_POST['app_lock_wait']<1)                                                                  { $error[] = "Invalid wait value"; }
 	}
 	# api_allow_unsafe check
-	if($_POST['app_security']=="none" && Config::get('api_allow_unsafe')!==true)											{ $error[] = "API server requires SSL. Please set $api_allow_unsafe in config.php to override"; }
+	if($_POST['app_security']=="none" && Config::get('api_allow_unsafe')!==true)											{ $error[] = "API server requires SSL. Please set \$api_allow_unsafe in config.php to override"; }
 }
 
 # default lock_wait
