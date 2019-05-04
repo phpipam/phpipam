7f1a3dbad0527908e7bcb9994dd2c90aa3454998
Gary Allan <github@gallan.co.uk>
2019-05-04 15:52:27 +0100

    Bugfix: isFolder can be {0,1,NULL}. Fix logic checks.

--- app/admin/routing/edit-bgp-mapping-search.php
+++ app/admin/routing/edit-bgp-mapping-search.php
@@ -21,7 +21,7 @@ if(!is_numeric($_POST['bgp_id']))  			{ $Result->show("danger",  _("Invalid ID")
 if(strlen($_POST['subnet'])<2)				{ $Result->show("danger",  _("Please enter at least 2 characters."), true); }
 
 # query
-$query = 'select INET_NTOA(`subnet`) as subnet,id,mask,description from `subnets` where INET_NTOA(`subnet`) like "'.$_POST['subnet'].'%" and `subnet` > 1 and `isFolder` = 0';
+$query = 'select INET_NTOA(`subnet`) as subnet,id,mask,description from `subnets` where INET_NTOA(`subnet`) like "'.$_POST['subnet'].'%" and `subnet` > 1 and COALESCE(`isFolder`,0) = 0';
 # execute
 try { $subnets = $Database->getObjectsQuery($query); }
 catch (Exception $e) {
