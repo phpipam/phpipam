f29328e8d2b12febf715cb7d26dee194f609afc0
Miha Petkovsek <miha.petkovsek@gmail.com>
2019-07-02 09:40:04 +0200

    Added PUBLISHED flag to hide dbversion in footer

--- app/footer.php
+++ app/footer.php
@@ -1,7 +1,8 @@
 <table class="donate">
 <tr>
 	<td>
-		<a href="http://phpipam.net">phpIPAM IP address management <?php print '[v'. VERSION_VISIBLE. ']'; ?><?php if(DBVERSION > 0) { print " dbversion".DBVERSION; } ?></a>
+		<?php var_dump(PUBLISHED); ?>
+		<a href="http://phpipam.net">phpIPAM IP address management <?php print '[v'. VERSION_VISIBLE. ']'; ?><?php if(PUBLISHED===false) { print " dbversion".DBVERSION; } ?></a>
 	</td>
 
 	<?php
