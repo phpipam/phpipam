aa5fb1dffb4b6829679ed899e87cf79fe9406ba1
Stany MARCEL <stanypub@gmail.com>
2019-07-11 00:26:01 +0200

    Add search user before bind test for AD login
    With some AD the samAccountName can't be used directly for bind,
    so it is preferable to search the user to get it's DN for bind
    
    Signed-off-by: Stany MARCEL <stanypub@gmail.com>

--- functions/classes/class.User.php
+++ functions/classes/class.User.php
@@ -986,6 +986,10 @@ class User extends Common_functions {
         else {
             $dirparams['use_ssl'] = @$authparams['use_ssl'];
             $dirparams['use_tls'] = @$authparams['use_tls'];
+            if (isset($authparams['adminUsername']) && isset($authparams['adminPassword'])) {
+                $dirparams['admin_username'] = $authparams['adminUsername'];
+                $dirparams['admin_password'] = $authparams['adminPassword'];
+            }
         }
         # open connection
         try {
@@ -1009,9 +1013,10 @@ class User extends Common_functions {
      * @param array $authparams
      * @param string $username
      * @param string $password
+     * @param boolean $search
      * @return void
      */
-    private function directory_authenticate ($authparams, $username, $password) {
+    private function directory_authenticate ($authparams, $username, $password, $search) {
         // set method
         $method = $this->ldap ? "LDAP" : "AD";
         // connect
@@ -1019,6 +1024,21 @@ class User extends Common_functions {
 
         # authenticate
         try {
+
+            if ($search && isset($authparams['adminUsername']) && isset($authparams['adminPassword'])) {
+                // bind with settings user
+                $adldap->authenticate($authparams->adminUsername, $authparams->adminPassword);
+
+                // search the user to get it's DN
+                $userinfo = $adldap->user()->info($username, array("*"), false);
+                if ($userinfo !== false) {
+                    // found so lets replace the user by the DN
+                    $username = $userinfo[0]['distinguishedname'][0];
+                } else {
+                    $this->Log->write($method . " login", "Unable to get user info for " . $username, 1, $username);
+                }
+            }
+
             if ($adldap->authenticate($username, $password)) {
                 # save to session
                 $this->write_session_parameters();
@@ -1057,7 +1077,7 @@ class User extends Common_functions {
         // parse settings for LDAP connection and store them to array
         $authparams = json_decode($this->authmethodparams, true);
         // authenticate
-        $this->directory_authenticate($authparams, $username, $password);
+        $this->directory_authenticate($authparams, $username, $password, true);
     }
 
     /**
@@ -1081,7 +1101,7 @@ class User extends Common_functions {
         if (!empty($authparams['users_base_dn'])) { $udn = $udn . "," . $authparams['users_base_dn']; }
         else                                      { $udn = $udn . "," . $authparams['base_dn']; }
         // authenticate
-        $this->directory_authenticate($authparams, $udn, $password);
+        $this->directory_authenticate($authparams, $udn, $password, false);
     }
 
     /**
