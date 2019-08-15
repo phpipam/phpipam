f9a6fb907baea876316ab99bdbced1ca93d18da3
Alejandro Gonzalez Perez <alejandro.gonzalez.2016@my.bristol.ac.uk>
2019-08-15 12:38:49 +0100

    Updated db

--- functions/upgrade_queries/upgrade_queries_1.5.php
+++ functions/upgrade_queries/upgrade_queries_1.5.php
@@ -8,4 +8,7 @@
 $upgrade_queries["1.5.26"][] = "ALTER TABLE `customers` CHANGE `postcode` `postcode` VARCHAR(32)  NULL  DEFAULT NULL;";
 $upgrade_queries["1.5.26"][] = "-- Database version bump";
 $upgrade_queries["1.5.26"][] = "UPDATE `settings` set `dbversion` = '26';";
+
 $upgrade_queries["1.5.27"][] = "ALTER TABLE `subnets` ADD `DNSforward` TINYINT(1)  NULL  DEFAULT '0';";
+$upgrade_queries["1.5.27"][] = "-- Database version bump";
+$upgrade_queries["1.5.27"][] = "UPDATE `settings` set `dbversion` = '27';";
