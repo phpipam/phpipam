dc3807500747c6bb0d15f8ab9525d4bd37fef56c
Gary Allan <github@gallan.co.uk>
2019-11-24 20:59:25 +0000

    Bugfix: Check for 'ob_gzhandler'. Fixes #2843

--- functions/output_buffering.php
+++ functions/output_buffering.php
@@ -1,7 +1,7 @@
 <?php
 
 function outputbuf_send() {
-	ob_end_flush();
+    ob_end_flush();
 }
 
 // Don't enable output buffering for cli
@@ -9,7 +9,8 @@ if (php_sapi_name()!=="cli") {
 
     // Buffer output and send gzcompressed at script completion.
     // Improves page loading times on low bandwith/high latency connections.
-    if(!ob_start("ob_gzhandler")) ob_start();
+    if(!function_exists('ob_gzhandler') || !ob_start('ob_gzhandler'))
+        ob_start();
 
     // send buffer on program completion.
     register_shutdown_function('outputbuf_send');
