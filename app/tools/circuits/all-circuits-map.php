28522a75daba4ef64fdbba39b7bf553da031b759
Gary Allan <github@gallan.co.uk>
2020-03-01 16:10:17 +0000

    Bugfix: PHP object injection. Fixes #2937
        $circuits_to_map = unserialize($_GET['circuits_to_map']);
    
    Code appears to be for a planned feature to map a subset
    of circuits. Use PHP object injection safe json_decode().

--- app/tools/circuits/all-circuits-map.php
+++ app/tools/circuits/all-circuits-map.php
@@ -10,7 +10,7 @@ $User->check_module_permissions ("circuits", 1, true, false);
 # title
 if(isset($_GET['map_specific']) && $_GET['map_specific'] == 'true'){
     print "<h3>"._('Map of circuits')."</h3>";
-    $circuits_to_map = unserialize($_GET['circuits_to_map']);
+    $circuits_to_map = json_decode($_GET['circuits_to_map'], true);
 }else{
     print "<h3>"._('Map of all circuits')."</h3>";
 }
