9c66aede6d90fb614c1a5efcbaad00d4c8a45ba8
Richard C. Greenwood <rgreenwood at shastacoe.org>
2020-03-05 15:27:04 -0800

    Use transform_to_dotted instead of long2ip4 to handle IPv4 and IPv6 addresses.

--- app/subnets/addresses/print-address-table.php
+++ app/subnets/addresses/print-address-table.php
@@ -272,7 +272,7 @@ else {
                     // search for hostname records
 					$records = $PowerDNS->search_records ("name", $addresses[$n]->hostname, 'name', true);
 					$ptr	 = $PowerDNS->fetch_record ($addresses[$n]->PTR);
-					$ptr_name = $PowerDNS->get_ip_ptr_name($Tools->long2ip4($addresses[$n]->ip_addr));
+					$ptr_name = $PowerDNS->get_ip_ptr_name($Tools->transform_to_dotted($addresses[$n]->ip_addr));
 					if(! $ptr || $ptr_name != $ptr->name) {
 					        $ptr = $PowerDNS->search_records("name", $ptr_name);
 					        if($ptr) {
