59eaad0a9d3dd063fe603680e2027b8fb9626788
David Dolgin <David@Daviddolgin.com>
2020-10-12 21:43:26 -0700

    chmoding upload directory for import to work

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,4 +25,7 @@ exportsecrets $VAULT_KV_PATH
 
 ln -s config.docker.php config.php
 
+#fix import permission issue with a chmod
+chmod a+w /phpipam/app/admin/import-export/upload
+
 /sbin/tini -- /bin/sh -c /start_apache2
