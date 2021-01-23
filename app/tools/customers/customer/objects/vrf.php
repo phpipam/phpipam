d1a6f1e8137746360e69db8fe31f16d3d09fe227
Carsten Schoenert <c.schoenert@t-online.de>
2021-01-23 09:53:45 +0100

    l10n: making action menu translatable for customers view

--- app/tools/customers/customer/objects/vrf.php
+++ app/tools/customers/customer/objects/vrf.php
@@ -73,20 +73,20 @@ if (isset($objects["vrf"])) {
 		// actions
         print "<td class='actions'>";
         $links = [];
-        $links[] = ["type"=>"header", "text"=>"Show"];
-        $links[] = ["type"=>"link", "text"=>"Show VRF", "href"=>create_link($_GET['page'], "vrf", $vrf->vrfId), "icon"=>"eye", "visible"=>"dropdown"];
+        $links[] = ["type"=>"header", "text"=>_("Show")];
+        $links[] = ["type"=>"link", "text"=>_("Show VRF"), "href"=>create_link($_GET['page'], "vrf", $vrf->vrfId), "icon"=>"eye", "visible"=>"dropdown"];
         $links[] = ["type"=>"divider"];
         if($User->get_module_permissions ("vrf")>=User::ACCESS_RW) {
-            $links[] = ["type"=>"header", "text"=>"Manage"];
-            $links[] = ["type"=>"link", "text"=>"Edit VRF", "href"=>"", "class"=>"open_popup", "dataparams"=>" data-script='app/admin/vrf/edit.php' data-class='700' data-action='edit' data-vrfid='$vrf->vrfId'", "icon"=>"pencil"];
+            $links[] = ["type"=>"header", "text"=>_("Manage")];
+            $links[] = ["type"=>"link", "text"=>_("Edit VRF"), "href"=>"", "class"=>"open_popup", "dataparams"=>" data-script='app/admin/vrf/edit.php' data-class='700' data-action='edit' data-vrfid='$vrf->vrfId'", "icon"=>"pencil"];
         }
         if($User->get_module_permissions ("vrf")>=User::ACCESS_RWA) {
-            $links[] = ["type"=>"link", "text"=>"Delete VRF", "href"=>"", "class"=>"open_popup", "dataparams"=>" data-script='app/admin/vrf/edit.php' data-class='700' data-action='delete' data-vrfid='$vrf->vrfId'", "icon"=>"times"];
+            $links[] = ["type"=>"link", "text"=>_("Delete VRF"), "href"=>"", "class"=>"open_popup", "dataparams"=>" data-script='app/admin/vrf/edit.php' data-class='700' data-action='delete' data-vrfid='$vrf->vrfId'", "icon"=>"times"];
         }
 		if($User->get_module_permissions ("customers")>=User::ACCESS_RW) {
 	        $links[] = ["type"=>"divider"];
-            $links[] = ["type"=>"header", "text"=>"Unlink"];
-            $links[] = ["type"=>"link", "text"=>"Unlink object", "href"=>"", "class"=>"open_popup", "dataparams"=>" data-script='app/admin/customers/unlink.php' data-class='700' data-object='vrf' data-id='$vrf->vrfId'", "icon"=>"times"];
+            $links[] = ["type"=>"header", "text"=>_("Unlink")];
+            $links[] = ["type"=>"link", "text"=>_("Unlink object"), "href"=>"", "class"=>"open_popup", "dataparams"=>" data-script='app/admin/customers/unlink.php' data-class='700' data-object='vrf' data-id='$vrf->vrfId'", "icon"=>"times"];
 		}
         // print links
         print $User->print_actions($User->user->compress_actions, $links);
