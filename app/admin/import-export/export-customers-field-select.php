be278a5d8edecf6904f842e45c326d5ca5ab3e7e
Sean Cox <throwarock@gmail.com>
2021-10-18 10:31:50 -0700

    (Hopefully) working customers export

---
+++ app/admin/import-export/export-customers-field-select.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ *	Customers export
+ */
+
+# include required scripts
+require_once( dirname(__FILE__) . '/../../../functions/functions.php' );
+
+# initialize user object
+$Database	= new Database_PDO;
+$User		= new User ($Database);
+$Admin		= new Admin ($Database);
+$Tools		= new Tools ($Database);
+
+# verify that user is logged in
+$User->check_user_session();
+
+# Won't check per subnet/section rights since this is an admin section, where the admin user has full access
+
+# get all custom fields
+$custom_fields = $Tools->fetch_custom_fields('customers');
+# prepare HTML variables
+$custom_fields_names = "";
+$custom_fields_boxes = "";
+
+if(sizeof($custom_fields) > 0) {
+	foreach($custom_fields as $myField) {
+		//change spaces to "___" so it can be used as element id
+		$myField['nameTemp'] = str_replace(" ", "___", $myField['name']);
+		$custom_fields_names.= "	<th>$myField[name]</th>";
+		$custom_fields_boxes.= "	<td><input type='checkbox' name='$myField[nameTemp]' checked> </td>";
+	}
+}
+
+?>
+
+<!-- header -->
+<div class="pHeader"><?php print _("Select fields to export"); ?></div>
+
+<!-- content -->
+<div class="pContent">
+
+<?php
+
+# print
+print '<form id="selectExportFields">';
+print '<h4>Fields</h4>';
+# table
+print "	<table class='table table-striped table-condensed'>";
+
+print "	<tr>";
+print "	<th>"._('id')."</th>";
+print "	<th>"._('title')."</th>";
+print "	<th>"._('address')."</th>";
+print "	<th>"._('postcode')."</th>";
+print "	<th>"._('city')."</th>";
+print "	<th>"._('state')."</th>";
+print "	<th>"._('lat')."</th>";
+print "	<th>"._('long')."</th>";
+print "	<th>"._('contact_person')."</th>";
+print "	<th>"._('contact_phone')."</th>";
+print "	<th>"._('contact_mail')."</th>";
+print "	<th>"._('note')."</th>";
+print $custom_fields_names;
+print "	</tr>";
+
+print "	<tr>";
+print "	<td><input type='checkbox' name='id' checked></td>";
+print "	<td><input type='checkbox' name='title' checked title='"._('Mandatory')."'> </td>";
+print "	<td><input type='checkbox' name='address' checked > </td>";
+print "	<td><input type='checkbox' name='postcode' checked> </td>";
+print "	<td><input type='checkbox' name='city' checked></td>";
+print "	<td><input type='checkbox' name='state' checked></td>";
+print "	<td><input type='checkbox' name='lat'> </td>";
+print "	<td><input type='checkbox' name='long'> </td>";
+print "	<td><input type='checkbox' name='contact_person'> </td>";
+print "	<td><input type='checkbox' name='contact_phone'> </td>";
+print "	<td><input type='checkbox' name='contact_mail'> </td>";
+print "	<td><input type='checkbox' name='note'> </td>";
+print $custom_fields_boxes;
+print "	</tr>";
+
+print '</table>';
+print '</form>';
+
+
+?>
+
+</div>
+
+<!-- footer -->
+<div class="pFooter">
+	<div class="btn-group">
+		<button class="btn btn-sm btn-default hidePopups"><?php print _('Cancel'); ?></button>
+		<button class="btn btn-sm btn-success" id="dataExportSubmit" data-type="customers"><i class="fa fa-upload"></i> <?php print _('Export'); ?></button>
+	</div>
+</div>
--- app/admin/import-export/export-customers.php
+++ app/admin/import-export/export-customers.php
@@ -1,56 +1,48 @@
 <?php
 
-/**
- *	Generate XLS file for Subnets
- ************************************/
+/***
+ *	Generate XLS file for Customers
+ *********************************/
 
 # include required scripts
 require_once( dirname(__FILE__) . '/../../../functions/functions.php' );
+
+# Don't corrupt output with php errors!
+disable_php_errors();
+
 require( dirname(__FILE__) . '/../../../functions/PEAR/Spreadsheet/Excel/Writer.php');
 
-# initialize user object
-$Database 	= new Database_PDO;
-$User 		= new User ($Database);
-$Admin 		= new Admin ($Database);
-$Tools	    = new Tools ($Database);
-$Subnets	= new Subnets ($Database);
-$Sections	= new Sections ($Database);
+# initialize required objects
+$Database	= new Database_PDO;
+$Result		= new Result;
+$User		= new User ($Database);
+$Admin		= new Admin ($Database);
+$Tools		= new Tools ($Database);
+$Customers	= new Customers ($Database);
 
 # verify that user is logged in
 $User->check_user_session();
 
-# Won't check per subnet/section rights since this is an admin section, where the admin user has full access
+# get all custom fields
+$custom_fields = $Tools->fetch_custom_fields('customers');
+# prepare HTML variables
+$custom_fields_names = "";
+$custom_fields_boxes = "";
+$fields = array ( 'id', 'title', 'address', 'postcode', 'city', 'state', 'lat', 'long', 'contact_person', 'contact_phone', 'contact_mail', 'note' );
 
-# fetch all sections
-$all_sections = $Sections->fetch_all_sections();
-
-# Lets do some reordering to show slaves!
-if($all_sections !== false) {
-	foreach($all_sections as $s) {
-		if($s->masterSection=="0") {
-			# it is master
-			$s->class = "master";
-			$sectionssorted[] = $s;
-			# check for slaves
-			foreach($all_sections as $ss) {
-				if($ss->masterSection==$s->id) {
-					$ss->class = "slave";
-					$sectionssorted[] = $ss;
-				}
-			}
-		}
+if(sizeof($custom_fields) > 0) {
+	foreach($custom_fields as $myField) {
+		//change spaces to "___" so it can be used as element id
+		$myField['nameTemp'] = str_replace(" ", "___", $myField['name']);
+		array_push ( $fields, $myField['nameTemp'] );
 	}
-	# set new array
-	$sections_sorted = @$sectionssorted;
 }
 
-
-# get all custom fields
-$custom_fields = $Tools->fetch_custom_fields('subnets');
+$customers = $Customers->fetch_all_objects("customers", "id");
 
 # Create a workbook
 $today = date("Ymd");
-$filename = $today."_phpipam_subnets_export.xls";
+$filename = $today."_phpipam_deviceTypes_export.xls";
 $workbook = new Spreadsheet_Excel_Writer();
 $workbook->setVersion(8);
 
@@ -65,251 +57,45 @@ $format_header->setAlign('left');
 $format_text =& $workbook->addFormat();
 
 // Create a worksheet
-$worksheet_name = "Subnets";
+$worksheet_name = "devtypes domains";
 $worksheet =& $workbook->addWorksheet($worksheet_name);
 $worksheet->setInputEncoding("utf-8");
 
 $curRow = 0;
 $curColumn = 0;
 
-//write headers
-if( (isset($_GET['section'])) && ($_GET['section'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('Section') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['subnet'])) && ($_GET['subnet'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('Subnet') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['description'])) && ($_GET['description'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('Description') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['VLAN'])) && ($_GET['VLAN'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('VLAN') ,$format_header);
-	$curColumn++;
-	$worksheet->write($curRow, $curColumn, _('VLAN Domain') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['VRF'])) && ($_GET['VRF'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('VRF') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['master'])) && ($_GET['master'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('Master Subnet') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['requests'])) && ($_GET['requests'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('Requests') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['hostscheck'])) && ($_GET['hostscheck'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('Host check') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['discover'])) && ($_GET['discover'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('Discover') ,$format_header);
-	$curColumn++;
-}
-if( (isset($_GET['customer'])) && ($_GET['customer'] == "on") ) {
-	$worksheet->write($curRow, $curColumn, _('Customer') ,$format_header);
-	$curColumn++;
+
+
+foreach ($fields as $k) {
+    if( ($_GET[$k] == "on") ) {
+        $worksheet->write($curRow, $curColumn, _($k) ,$format_header);
+        $curColumn++;
+    }
 }
 
-//custom fields
-if(sizeof($custom_fields) > 0) {
-	foreach($custom_fields as $myField) {
-		//set temp name - replace space with three ___
-		$myField['nameTemp'] = str_replace(" ", "___", $myField['name']);
-
-		if( (isset($_GET[$myField['nameTemp']])) && ($_GET[$myField['nameTemp']] == "on") ) {
-			$worksheet->write($curRow, $curColumn, $myField['name'] ,$format_header);
-			$curColumn++;
-		}
-	}
-}
-
+$curRow++;
 $curColumn = 0;
-$curRow++;
 
-//write Subnet entries for the selected sections
-if($all_sections!==false) {
-	foreach ($all_sections as $section) {
-		//cast
-		$section = (array) $section;
-		$section['url_name'] = urlencode($section['id']);
+foreach ($devices as $d) {
+	//cast
+	$d = (array) $d;
 
-		if( (isset($_GET['exportSection__'.$section['url_name']])) && ($_GET['exportSection__'.$section['url_name']] == "on") ) {
-			// get all subnets in section
-			$section_subnets = $Subnets->fetch_section_subnets($section['id']);
-
-			if (!is_array($section_subnets)) { continue; }
-
-			foreach ($section_subnets as $subnet) {
-
-				$subnet = (array) $subnet;
-
-				if( (isset($_GET['section'])) && ($_GET['section'] == "on") ) {
-					$worksheet->write($curRow, $curColumn, $section['name'], $format_text);
-					$curColumn++;
-				}
-
-				if( (isset($_GET['subnet'])) && ($_GET['subnet'] == "on") ) {
-					$subnet_text = '';
-                                        if ($subnet['isFolder']) {
-						$subnet_text = $subnet['description']." (folder)";
-					} else {
-						$subnet_text = $subnet['ip']."/".$subnet['mask'];
-					}
-					$worksheet->write($curRow, $curColumn, $subnet_text, $format_text);
-					$curColumn++;
-				}
-
-				if( (isset($_GET['description'])) && ($_GET['description'] == "on") ) {
-					$worksheet->write($curRow, $curColumn, $subnet['description'], $format_text);
-					$curColumn++;
-				}
-
-				if( (isset($_GET['VLAN'])) && ($_GET['VLAN'] == "on") ) {
-					// get VLAN
-					$vlan = (array) $Tools->fetch_object("vlans", "vlanId", $subnet['vlanId']);
-					/* if(@$vlan[0]===false) 	{ $vlan['number'] = "NA"; $vlan['name'] = "NA"; }			# no VLAN
-					$worksheet->write($curRow, $curColumn, $vlan['number']." [".$vlan['name']."]", $format_text); */
-					if(@$vlan[0]===false) 	{ $vlan['number'] = "NA"; }			# no VLAN
-					$worksheet->write($curRow, $curColumn, $vlan['number'], $format_text);
-					$curColumn++;
-					// VLAN Domain
-					$vlan_domain = (array) $Tools->fetch_object("vlanDomains", "id", $vlan['domainId']);
-					$worksheet->write($curRow, $curColumn, $vlan_domain['name'], $format_text);
-					$curColumn++;
-				}
-
-				if( (isset($_GET['VRF'])) && ($_GET['VRF'] == "on") ) {
-					// get vrf
-					if (!empty($subnet['vrfId'])) {
-						$vrf = (array) $Tools->fetch_object("vrf", "vrfId", $subnet['vrfId']);
-						$worksheet->write($curRow, $curColumn, $vrf['name'], $format_text);
-					} else {
-						$worksheet->write($curRow, $curColumn, '', $format_text);
-					}
-					$curColumn++;
-				}
-
-				if( (isset($_GET['master'])) && ($_GET['master'] == "on") ) {
-					// get master subnet
-					// zet - could optimize here and reference the already loaded subnets, with the help of a dictionary variable
-					$masterSubnet = ( $subnet['masterSubnetId']==0 || empty($subnet['masterSubnetId']) ) ? false : true;
-					if($masterSubnet) {
-						$master = (array) $Subnets->fetch_subnet (null, $subnet['masterSubnetId']);
-						if($master['isFolder']) {
-							$worksheet->write($curRow, $curColumn, $master['description']." [folder]", $format_text);
-						} else {
-							$worksheet->write($curRow, $curColumn, $master['ip']."/".$master['mask'], $format_text);
-						}
-					} else {
-						$worksheet->write($curRow, $curColumn, "/", $format_text);
-					}
-					$curColumn++;
-				}
-
-				if( (isset($_GET['requests'])) && ($_GET['requests'] == "on") ) {
-					$worksheet->write($curRow, $curColumn, $subnet['allowRequests'], $format_text);
-					$curColumn++;
-				}
-
-				if( (isset($_GET['hostscheck'])) && ($_GET['hostscheck'] == "on") ) {
-					$worksheet->write($curRow, $curColumn, $subnet['pingSubnet'], $format_text);
-					$curColumn++;
-				}
-
-				if( (isset($_GET['discover'])) && ($_GET['discover'] == "on") ) {
-					$worksheet->write($curRow, $curColumn, $subnet['discoverSubnet'], $format_text);
-					$curColumn++;
-				}
-
-				if( (isset($_GET['customer'])) && ($_GET['customer'] == "on") ) {
-					// get customer
-					if (!empty($subnet['customer_id'])) {
-						$customer = (array) $Tools->fetch_object("customers", "id", $subnet['customer_id']);
-						$worksheet->write($curRow, $curColumn, $customer['title'], $format_text);
-					} else {
-						$worksheet->write($curRow, $curColumn, '', $format_text);
-					}
-					$curColumn++;
-				}
-
-				//custom fields, per subnet
-				if(sizeof($custom_fields) > 0) {
-					foreach($custom_fields as $myField) {
-						//set temp name - replace space with three ___
-						$myField['nameTemp'] = str_replace(" ", "___", $myField['name']);
-
-						if( (isset($_GET[$myField['nameTemp']])) && ($_GET[$myField['nameTemp']] == "on") ) {
-							$worksheet->write($curRow, $curColumn, $subnet[$myField['name']], $format_text);
-							$curColumn++;
-						}
-					}
-				}
-
-			//reset row count
-			$curColumn = 0;
-			$curRow++;
-			}
-		}
-	}
-}
-
-//new line
-$curRow++;
-
-//write section sheet
-if( (isset($_GET['exportSections'])) && ($_GET['exportSections'] == "on") ) {
-	// Create a worksheet
-	$worksheet_sections =& $workbook->addWorksheet('Sections');
-
-	$curRow = 0;
-	$curColumn = 0;
-
-	//write headers
-	$worksheet_sections->write($curRow, $curColumn, _('Name') ,$format_header);
-	$curColumn++;
-	$worksheet_sections->write($curRow, $curColumn, _('Description') ,$format_header);
-	$curColumn++;
-	$worksheet_sections->write($curRow, $curColumn, _('Parent') ,$format_header);
-	$curColumn++;
+    foreach ($fields as $k) {
+        if( (isset($_GET[$k])) && ($_GET[$k] == "on") ) {
+            $worksheet->write($curRow, $curColumn, $d[$k], $format_text);
+            $curColumn++;
+        }
+    }
 
 	$curRow++;
 	$curColumn = 0;
-
-	foreach ($sections_sorted as $section) {
-		//cast
-		$section = (array) $section;
-		$section['url_name'] = urlencode($section['id']);
-
-		if( (isset($_GET['exportSection__'.$section['url_name']])) && ($_GET['exportSection__'.$section['url_name']] == "on") ) {
-			$worksheet_sections->write($curRow, $curColumn, $section['name'], $format_text);
-			$curColumn++;
-			$worksheet_sections->write($curRow, $curColumn, $section['description'], $format_text);
-			$curColumn++;
-			//master Section
-			if($section['masterSection']!=0) {
-				# get section details
-				$ssec = $Admin->fetch_object("sections", "id", $section['masterSection']);
-				$worksheet_sections->write($curRow, $curColumn, $ssec->name, $format_text);
-				$curColumn++;
-			} else {
-				$worksheet_sections->write($curRow, $curColumn, "/", $format_text);
-				$curColumn++;
-			}
-		}
-
-		$curRow++;
-		$curColumn = 0;
-	}
 }
+
 
 // sending HTTP headers
 $workbook->send($filename);
 
 // Let's send the file
 $workbook->close();
+
+?>
