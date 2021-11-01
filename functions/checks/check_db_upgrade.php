752c9761e9ba4da91cd26d30ce8273d8211fbe5e
Gary Allan <github@gallan.co.uk>
2021-11-01 21:26:23 +0000

    Bugfix: Database stored sessions require the php_sessions table to exist (dbversion>=3)
    Base table or view not found: 1146 Table 'phpipam.php_sessions' doesn't exist when upgrading from v1.3.2 #3417

--- functions/checks/check_db_upgrade.php
+++ functions/checks/check_db_upgrade.php
@@ -4,11 +4,6 @@
  * Check if database needs upgrade to newer version
  ****************************************************/
 
-# use required functions
-if(!isset($User->settings->dbversion)) {
-	$User->settings->dbversion = 0;
-}
-
 /* redirect */
 if($User->cmp_version_strings($User->settings->version.'.'.$User->settings->dbversion,VERSION.'.'.DBVERSION) < 0) {
 	$User->settings->prettyLinks="No";
