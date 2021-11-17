fbd7a5861d4757775b2416c0e30d6e8c9eee3b2c
Gary Allan <github@gallan.co.uk>
2021-11-17 21:24:47 +0000

    magic.js: RFC 6265 cookies. Fixes #3452

--- js/magic.js
+++ js/magic.js
@@ -215,28 +215,34 @@ $(document).on("click", ".selfDestruct", function() {
 
 
 /* @cookies */
-function createCookie(name,value,days) {
+function createCookie(name, value, days) {
     var date;
     var expires;
 
-    if (typeof days !== 'undefined') {
+    if (days === undefined) {
+        expires = "";
+    } else {
         date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        expires = "; expires="+date.toGMTString();
-    }
-    else {
-        var expires = "";
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
     }
 
-    document.cookie = name+"="+value+expires+"; path=/";
+    value = encodeURIComponent(value);
+    document.cookie = name + "=" + value + expires + "; path=/; SameSite=Lax";
 }
 function readCookie(name) {
     var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+    var ca = document.cookie.split(";");
+    var i;
+    var c;
+    for (i = 0; i < ca.length; i += 1) {
+        c = ca[i];
+        while (c.charAt(0) === " ") {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return decodeURIComponent(c.substring(nameEQ.length, c.length));
+        }
     }
     return null;
 }
