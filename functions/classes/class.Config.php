0ba2fd95b1f4e20e8d5f8b404a03ae3a7876b441
Gary Allan <github@gallan.co.uk>
2022-04-01 21:57:15 +0000

    Docker: Add IPAM_CONFIG_FILE config.php env

--- functions/classes/class.Config.php
+++ functions/classes/class.Config.php
@@ -14,11 +14,26 @@ class Config {
     private static $config = null;
 
     /**
+     * Return path of config.php
+     *
+     * @return string
+     */
+    private static function config_file_path() {
+        // IPAM_CONFIG_FILE, alternative config.php location
+        $alt_file = getenv('IPAM_CONFIG_FILE');
+        if (is_string($alt_file) && preg_match('/\.php$/', $alt_file) && is_readable($alt_file)) {
+            return $alt_file;
+        }
+
+        return dirname(__FILE__) . "/../../config.php";
+    }
+
+    /**
      * Read config.php info self::$config
      * @return void
      */
     private static function read_config() {
-        require( dirname(__FILE__)."/../../config.php" );
+        require(self::config_file_path());
         self::$config = (object) get_defined_vars();
     }
 
