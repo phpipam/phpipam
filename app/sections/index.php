099b5b6f895ff83b88b208dcab57500ea3a92c14
chuck-crane <chuck.c.crane@gmail.com>
2022-09-02 11:55:46 -0700

    mt5

--- app/sections/index.php
+++ app/sections/index.php
@@ -54,7 +54,7 @@ if ($User->settings->enableIPrequests==1) {
 
 # get admin and tools menu items
 //require( dirname(__FILE__) . '/../tools/tools-menu-config.php' );
-//require( dirname(__FILE__) . '/../admin/admin-menu-config.php' );
+require( dirname(__FILE__) . '/../admin/admin-menu-config.php' );
 
 ?>
 
