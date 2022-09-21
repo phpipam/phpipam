5199be28a746f1a70ed5ed9c8cc9cb165d164e9a
chuck-crane <chuck.c.crane@gmail.com>
2022-09-21 12:33:28 -0700

    test die

--- index.php
+++ index.php
@@ -307,14 +307,14 @@ else {
 						if (!isset($_GET['section']))										{ include("app/admin/index.php"); }
 						elseif (@$_GET['subnetId']=="section-changelog")					{ include("app/sections/section-changelog.php"); }
 						else {print_r ($admin_menu_items);print"<p>";
-	                        if (!isset($admin_menu_items[$_GET['section']]))             { print "<h4>Here</h4>";header("Location: ".create_link("error","400")); die(); }
+	                        if (!isset($admin_menu_items[$_GET['section']]))             { print "<h4>Here</h4>";die;header("Location: ".create_link("error","400")); die(); }
 							elseif(!file_exists("app/admin/".$_GET['section']."/index.php")) 		{ header("Location: ".create_link("error","404")); die(); }
 							else 															{ include("app/admin/$_GET[section]/index.php"); }
 						}
 					}
 					# default - error
 					else {
-																							{ print "<h4>Other</h4>";header("Location: ".create_link("error","400")); die(); }
+																							{ print "<h4>Other</h4>";die;header("Location: ".create_link("error","400")); die(); }
 					}
 				print "</div>";
 				print "</td>";
