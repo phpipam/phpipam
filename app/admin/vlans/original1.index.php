7411ad49848ffc78581e19f35c6ebc686a3da4d4
chuck-crane <chuck.c.crane@gmail.com>
2022-09-21 14:52:54 -0700

    s

--- app/admin/vlans/original1.index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * Script to display available VLANs
- */
-
-include(dirname(__FILE__) . "/../../tools/vlan/index.php");
--- app/tools/subnets/original1.index.php
+++
@@ -1,2 +0,0 @@
-<?php
-require( dirname(__FILE__) . '/../../admin/subnets/index.php');
