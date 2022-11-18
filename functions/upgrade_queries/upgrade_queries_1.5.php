bfbf855bf5f74e993ef42d6b82a2bbe561aef385
Gary Allan <github@gallan.co.uk>
2022-11-18 20:40:39 +0000

    Bugfix: Set version=1.5 in upgrade scripts.

--- functions/upgrade_queries/upgrade_queries_1.5.php
+++ functions/upgrade_queries/upgrade_queries_1.5.php
@@ -6,6 +6,8 @@
 
 // fix for postcode
 $upgrade_queries["1.5.26"]   = [];
+$upgrade_queries["1.5.26"][] =  "-- Version update";
+$upgrade_queries["1.5.26"][] = "UPDATE `settings` set `version` = '1.5';";
 $upgrade_queries["1.5.26"][] = "ALTER TABLE `customers` CHANGE `postcode` `postcode` VARCHAR(32)  NULL  DEFAULT NULL;";
 $upgrade_queries["1.5.26"][] = "-- Database version bump";
 $upgrade_queries["1.5.26"][] = "UPDATE `settings` set `dbversion` = '26';";
