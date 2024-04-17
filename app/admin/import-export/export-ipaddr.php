21a6e08eeb83424517b96ffa2917de5c583d77ff
Thomas Tsiakalakis <thomas.tsiakalakis@gdv-dl.de>
2024-04-17 11:17:48 +0200

    add location field to ip address export

--- app/admin/import-export/export-ipaddr.php
+++ app/admin/import-export/export-ipaddr.php
@@ -124,6 +124,18 @@ if( (isset($_GET['device'])) && ($_GET['device'] == "on") ) {
     	}
 	}
 }
+if( (isset($_GET['location'])) && ($_GET['location'] == "on") ) {
+	$worksheet->write($curRow, $curColumn, _('Location') ,$format_header);
+	$curColumn++;
+	# get Locations and reorder
+	$locations = $Tools->fetch_all_objects ("locations", "name");
+	$locations_indexed = array();
+	if ($locations!==false) {
+	foreach($locations as $d) {
+    		$locations_indexed[$d->id] = $d;
+    	}
+	}
+}
 if( (isset($_GET['note'])) && ($_GET['note'] == "on") ) {
 	$worksheet->write($curRow, $curColumn, _('Note') ,$format_header);
 	$curColumn++;
@@ -238,6 +250,13 @@ if($all_sections!==false) {
 						//change device to name
 						$ip['device'] = is_null($ip['switch'])||is_blank($ip['switch'])||$ip['switch']==0 ? "" : $devices_indexed[$ip['switch']]->hostname;
 						$worksheet->write($curRow, $curColumn, $ip['device'], $format_text);
+						$curColumn++;
+					}
+
+					if( (isset($_GET['location'])) && ($_GET['location'] == "on") ) {
+						//change location to name
+						$ip['location'] = is_null($ip['location'])||is_blank($ip['location'])||$ip['location']==0 ? "" : $locations_indexed[$ip['switch']]->hostname;
+						$worksheet->write($curRow, $curColumn, $ip['location'], $format_text);
 						$curColumn++;
 					}
 
