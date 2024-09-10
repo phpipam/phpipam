4bf93e864789e7d4e5ab6f7879a0f85464f1773c
riversdev0 <riversdev0@example.local>
2024-09-10 23:50:43 +0000

    fixed custom fields being disabled

--- app/admin/vaults/edit.php
+++ app/admin/vaults/edit.php
@@ -118,9 +118,9 @@ $custom = $Tools->fetch_custom_fields('vaults');
 		# all my fields
 		foreach($custom as $field) {
     		// create input > result is array (required, input(html), timepicker_index)
-    		$custom_input = $Tools->create_custom_field_input ($field, $vault, $_POST['action'], $timepicker_index);
+    		$custom_input = $Tools->create_custom_field_input ($field, $vault, $timepicker_index);
     		// add datepicker index
-    		$timepicker_index = $timepicker_index++;
+    		$timepicker_index++;
             // print
 			print "<tr>";
 			print "	<td>".ucwords($Tools->print_custom_field_name ($field['name']))." ".$custom_input['required']."</td>";
