fcfeaf9dc3aa6eb1e0fa54d26b9ccf974d6b6a5d
riversdev0 <riversdev0@example.local>
2025-03-23 01:50:00 +0000

    updated schema to be compatible

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -541,7 +541,7 @@ DROP TABLE IF EXISTS `changelog`;
 
 CREATE TABLE `changelog` (
   `cid` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `ctype` set('ip_addr','subnet','section') NOT NULL DEFAULT '',
+  `ctype` set('ip_addr','subnet','section','devices','vrf','vlans') NOT NULL DEFAULT '',
   `coid` int(11) unsigned NOT NULL,
   `cuser` int(11) unsigned NOT NULL,
   `caction` ENUM('add','edit','delete','truncate','resize','perm_change') NOT NULL DEFAULT 'edit',
