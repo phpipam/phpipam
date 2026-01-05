phpipam/phpipam

commit d3d51b6e02285846cef979fcc5a2661626a76d2d

php8: Compatibility fixes. Safer $_POST argument parsing.

app/admin/powerDNS/record-edit.php
@@ -18,7 +18,7 @@ $PowerDNS 	= new PowerDNS ($Database);
 # verify that user is logged in
 $User->check_user_session();
 # perm check popup
-if($_POST['action']=="edit") {
+if($POST->action=="edit") {
     $User->check_module_permissions ("pdns", User::ACCESS_RW, true, true);
 }
 else {
@@ -31,28 +31,25 @@ $csrf = $User->Crypto->csrf_cookie ("create", "record");
 # save settings for powerDNS default
 $pdns = $PowerDNS->db_settings;
 
-# default post
-$post = $_POST;
-
 # validate action
 $Admin->validate_action();
 
 # get record
-if($_POST['action']!="add") {
-	$record = $PowerDNS->fetch_record ($_POST['id']);
+if($POST->action!="add") {
+	$record = $PowerDNS->fetch_record ($POST->id);
 	$record!==false ? : $Result->show("danger", _("Invalid ID"), true, true);
 }
 # new record
 else {
 	// from IP table
 	// we provide record hostname and strip domain from it
-	if (!is_numeric($_POST['domain_id']) && !is_numeric($_POST['id'])) {
+	if (!is_numeric($POST->domain_id) && !is_numeric($POST->id)) {
 		// fetch all domains
 		$all_domains = $PowerDNS->fetch_all_domains ();
 		if ($all_domains!==false) {
 
 			// Reverse the hostname, this fixes #1471 and #2374
-			$r_hostdomain = implode(".", array_reverse(array_slice(pf_explode(".", $_POST['domain_id']), 1)));
+			$r_hostdomain = implode(".", array_reverse(array_slice(pf_explode(".", $POST->domain_id), 1)));
 
 			foreach($all_domains as $dk=>$domain_s) {
 				// Reverse the domain and compare it reversed, this fixes #1471 and #2374
@@ -69,30 +66,30 @@ else {
 					if($length > $max){ $max = $length; $element_id = $k; }
 				}
 				// save longest match id
-				$_POST['domain_id'] = $all_domains[$element_id]->id;
+				$POST->domain_id = $all_domains[$element_id]->id;
 			}
 		}
 		// die if not existing
-		if (!is_numeric($_POST['domain_id'])) {
+		if (!is_numeric($POST->domain_id)) {
     		# admin?
     		if ($User->is_admin()) {
-    			$Result->show("danger", _("Domain")." <strong>".escape_input($_POST['domain_id'])."</strong><span class='ip_dns_addr hidden'>".escape_input($_POST['id'])."</span> "._("does not exist")."!"."<hr><button class='btn btn-default btn-xs open_popup' data-script='app/admin/powerDNS/domain-edit.php' data-class='700' data-action='add' data-id='0' data-secondary='true'><i class='fa fa-plus'></i> "._('Create domain')."</button>", true, true);
+    			$Result->show("danger", _("Domain")." <strong>".escape_input($POST->domain_id)."</strong><span class='ip_dns_addr hidden'>".escape_input($POST->id)."</span> "._("does not exist")."!"."<hr><button class='btn btn-default btn-xs open_popup' data-script='app/admin/powerDNS/domain-edit.php' data-class='700' data-action='add' data-id='0' data-secondary='true'><i class='fa fa-plus'></i> "._('Create domain')."</button>", true, true);
     		}
     		else {
-    			$Result->show("danger", _("Domain")." <strong>".escape_input($_POST['domain_id'])."</strong> "._("does not exist")."!", true, true);
+    			$Result->show("danger", _("Domain")." <strong>".escape_input($POST->domain_id)."</strong> "._("does not exist")."!", true, true);
     		}
 		}
 		else {
 			$record = new StdClass ();
 			$record->ttl = (isset($pdns->ttl) && $pdns->ttl > 0) ? $pdns->ttl : 3600;
-			$record->name = $post['domain_id'];
-			$record->content = $_POST['id'];
+			$record->name = $POST->domain_id;
+			$record->content = $POST->id;
 		}
 	}
 }
 
 // get domain
-$domain = $PowerDNS->fetch_domain ($_POST['domain_id']);
+$domain = $PowerDNS->fetch_domain ($POST->domain_id);
 $domain!==false ? : $Result->show("danger", _("Invalid ID"), true, true);
 
 // default
@@ -108,7 +105,7 @@ if ($User->identify_address($record->content)=="IPv6" && $User->validate_ip($rec
 }
 
 # disable edit on delete
-$readonly = $_POST['action']=="delete" ? "readonly" : "";
+$readonly = $POST->action=="delete" ? "readonly" : "";
 ?>
 
 
@@ -126,9 +123,9 @@ $readonly = $_POST['action']=="delete" ? "readonly" : "";
 		<td style="width:150px;"><?php print _('Name'); ?></td>
 		<td>
 			<input type="text" class="name form-control input-sm" name="name" placeholder="<?php print _('www.example.com'); ?>" value="<?php print $record->name; ?>" <?php print $readonly; ?>>
-			<input type="hidden" name="action" value="<?php print escape_input($_POST['action']); ?>">
-			<input type="hidden" name="id" value="<?php print escape_input(@$_POST['id']); ?>">
-			<input type="hidden" name="domain_id" value="<?php print escape_input(@$_POST['domain_id']); ?>">
+			<input type="hidden" name="action" value="<?php print escape_input($POST->action); ?>">
+			<input type="hidden" name="id" value="<?php print escape_input($POST->id); ?>">
+			<input type="hidden" name="domain_id" value="<?php print escape_input($POST->domain_id); ?>">
             <input type="hidden" name="csrf_cookie" value="<?php print $csrf; ?>">
 		</td>
 	</tr>
@@ -209,10 +206,10 @@ $readonly = $_POST['action']=="delete" ? "readonly" : "";
 <div class="pFooter">
 	<div class="btn-group">
 		<button class="btn btn-sm btn-default hidePopups"><?php print _('Cancel'); ?></button>
-		<?php if($_POST['action']!=="delete" && isset($record->id) && $User->get_module_permissions ("pdns")>=User::ACCESS_RWA) { ?>
+		<?php if($POST->action!=="delete" && isset($record->id) && $User->get_module_permissions ("pdns")>=User::ACCESS_RWA) { ?>
 		<button class="btn btn-sm btn-default btn-danger" id="editRecordSubmitDelete"><i class="fa fa-trash-o"></i> <?php print _("Delete"); ?></button>
 		<?php } ?>
-		<button class="btn btn-sm btn-default <?php if($_POST['action']=="delete") { print "btn-danger"; } else { print "btn-success"; } ?>" id="editRecordSubmit"><i class="fa <?php if($_POST['action']=="add") { print "fa-plus"; } else if ($_POST['action']=="delete") { print "fa-trash-o"; } else { print "fa-check"; } ?>"></i> <?php print $User->get_post_action(); ?></button>
+		<button class="btn btn-sm btn-default <?php if($POST->action=="delete") { print "btn-danger"; } else { print "btn-success"; } ?>" id="editRecordSubmit"><i class="fa <?php if($POST->action=="add") { print "fa-plus"; } else if ($POST->action=="delete") { print "fa-trash-o"; } else { print "fa-check"; } ?>"></i> <?php print $User->get_post_action(); ?></button>
 	</div>
 	<!-- result -->
 	<div class="record-edit-result"></div>

app/admin/nameservers/edit-result.php
@@ -17,67 +17,63 @@ $Result 	= new Result ();
 $User->check_user_session();
 # check maintaneance mode
 $User->check_maintaneance_mode ();
-
-# strip input tags
-$_POST = $Admin->strip_input_tags($_POST);
-
 # validate csrf cookie
-$User->Crypto->csrf_cookie ("validate", "ns", $_POST['csrf_cookie']) === false ? $Result->show("danger", _("Invalid CSRF cookie"), true) : "";
+$User->Crypto->csrf_cookie ("validate", "ns", $POST->csrf_cookie) === false ? $Result->show("danger", _("Invalid CSRF cookie"), true) : "";
 
 
 # Name and primary nameserver must be present!
-if ($_POST['action']!="delete") {
+if ($POST->action!="delete") {
 
 	$m=1;
 	$nservers_reindexed = array ();
 	# reindex
-	foreach($_POST as $k=>$v) {
+	foreach($POST as $k=>$v) {
 		if(strpos($k, "namesrv-")!==false) {
 			$nservers_reindexed["namesrv-".$m] = $v;
 			$m++;
-			unset($_POST[$k]);
+			unset($POST->{$k});
 		}
 	}
 	# join
-	$_POST = array_merge($_POST, $nservers_reindexed);
+	$POST->read($nservers_reindexed);
 
-	if($_POST['name'] == "") 				{ $Result->show("danger", _("Name is mandatory"), true); }
-	if(trim($_POST['namesrv-1']) == "") 	{ $Result->show("danger", _("Primary nameserver is mandatory"), true); }
+	if($POST->name == "") 				{ $Result->show("danger", _("Name is mandatory"), true); }
+	if(trim($POST->namesrv-1) == "") 	{ $Result->show("danger", _("Primary nameserver is mandatory"), true); }
 }
 
 // merge nameservers
-foreach($_POST as $key=>$line) {
+foreach($POST as $key=>$line) {
 	if (!is_blank(strstr($key,"namesrv-"))) {
 		if (!is_blank($line)) {
 			$all_nameservers[] = trim($line);
 		}
 	}
 }
-$_POST['namesrv1'] = isset($all_nameservers) ? implode(";", $all_nameservers) : "";
+$POST->namesrv1 = isset($all_nameservers) ? implode(";", $all_nameservers) : "";
 
 // set sections
 $temp = array();
-foreach($_POST as $key=>$line) {
+foreach($POST as $key=>$line) {
 	if (!is_blank(strstr($key,"section-"))) {
 		$key2 = str_replace("section-", "", $key);
 		$temp[] = $key2;
-		unset($_POST[$key]);
+		unset($POST->{$key});
 	}
 }
 # glue sections together
-$_POST['permissions'] = sizeof($temp)>0 ? implode(";", $temp) : null;
+$POST->permissions = sizeof($temp)>0 ? implode(";", $temp) : null;
 
 # set update array
-$values = array("id"=>@$_POST['nameserverid'],
-				"name"=>$_POST['name'],
-				"permissions"=>$_POST['permissions'],
-				"namesrv1"=>$_POST['namesrv1'],
-				"description"=>$_POST['description']
+$values = array("id"=>$POST->nameserverid,
+				"name"=>$POST->name,
+				"permissions"=>$POST->permissions,
+				"namesrv1"=>$POST->namesrv1,
+				"description"=>$POST->description
 				);
 # update
-if(!$Admin->object_modify("nameservers", $_POST['action'], "id", $values))	{ $Result->show("danger", _("Failed to")." ".$User->get_post_action()." "._("nameserver set").'!', true); }
+if(!$Admin->object_modify("nameservers", $POST->action, "id", $values))	{ $Result->show("danger", _("Failed to")." ".$User->get_post_action()." "._("nameserver set").'!', true); }
 else { $Result->show("success", _("Nameserver set")." ".$User->get_post_action()." "._("successful").'!', false); }
 
 
 # remove all references if delete
-if($_POST['action']=="delete") { $Admin->remove_object_references ("subnets", "nameserverid", $_POST['nameserverid']); }
+if($POST->action=="delete") { $Admin->remove_object_references ("subnets", "nameserverid", $POST->nameserverid); }

app/admin/nameservers/edit.php
@@ -21,21 +21,18 @@ $User->check_user_session();
 # create csrf token
 $csrf = $User->Crypto->csrf_cookie ("create", "ns");
 
-# strip tags - XSS
-$_POST = $User->strip_input_tags ($_POST);
-
 # validate action
 $Admin->validate_action();
 
 # get Nameserver sets
-if($_POST['action']!="add") {
-	$nameservers = $Admin->fetch_object ("nameservers", "id", $_POST['nameserverid']);
+if($POST->action!="add") {
+	$nameservers = $Admin->fetch_object ("nameservers", "id", $POST->nameserverid);
 	$nameservers!==false ? : $Result->show("danger", _("Invalid ID"), true, true);
 	$nameservers = (array) $nameservers;
 }
 
 # disable edit on delete
-$readonly = $_POST['action']=="delete" ? "readonly" : "";
+$readonly = $POST->action=="delete" ? "readonly" : "";
 
 # set nameservers
 $nameservers['namesrv1'] = !isset($nameservers) ? array(" ") : pf_explode(";", $nameservers['namesrv1']);
@@ -96,9 +93,9 @@ $nameservers['namesrv1'] = !isset($nameservers) ? array(" ") : pf_explode(";", $
 		<td><?php print _('Description'); ?></td>
 		<td>
 			<?php
-			if( ($_POST['action'] == "edit") || ($_POST['action'] == "delete") ) { print '<input type="hidden" name="nameserverid" value="'. escape_input($_POST['nameserverid']) .'">'. "\n";}
+			if( ($POST->action == "edit") || ($POST->action == "delete") ) { print '<input type="hidden" name="nameserverid" value="'. escape_input($POST->nameserverid) .'">'. "\n";}
 			?>
-			<input type="hidden" name="action" value="<?php print escape_input($_POST['action']); ?>">
+			<input type="hidden" name="action" value="<?php print escape_input($POST->action); ?>">
 			<input type="hidden" name="csrf_cookie" value="<?php print $csrf; ?>">
 			<input type="text" class="description form-control input-sm" name="description" placeholder="<?php print _('Description'); ?>" value="<?php print $Tools->strip_xss(@$nameservers['description']); ?>" <?php print $readonly; ?>>
 		</td>
@@ -133,7 +130,7 @@ $nameservers['namesrv1'] = !isset($nameservers) ? array(" ") : pf_explode(";", $
 
 	<?php
 	//print delete warning
-	if($_POST['action'] == "delete")	{ $Result->show("warning", "<strong>"._('Warning').":</strong> "._("removing nameserver set will also remove all references from belonging subnets!"), false);}
+	if($POST->action == "delete")	{ $Result->show("warning", "<strong>"._('Warning').":</strong> "._("removing nameserver set will also remove all references from belonging subnets!"), false);}
 	?>
 </div>
 
@@ -142,8 +139,8 @@ $nameservers['namesrv1'] = !isset($nameservers) ? array(" ") : pf_explode(";", $
 <div class="pFooter">
 	<div class="btn-group">
 		<button class="btn btn-sm btn-default hidePopups"><?php print _('Cancel'); ?></button>
-		<button class='btn btn-sm btn-default submit_popup <?php if($_POST['action']=="delete") { print "btn-danger"; } else { print "btn-success"; } ?>' data-script="app/admin/nameservers/edit-result.php" data-result_div="nameserverManagementEditResult" data-form='nameserverManagementEdit'>
-			<i class="fa <?php if($_POST['action']=="add") { print "fa-plus"; } else if ($_POST['action']=="delete") { print "fa-trash-o"; } else { print "fa-check"; } ?>"></i> <?php print $User->get_post_action(); ?>
+		<button class='btn btn-sm btn-default submit_popup <?php if($POST->action=="delete") { print "btn-danger"; } else { print "btn-success"; } ?>' data-script="app/admin/nameservers/edit-result.php" data-result_div="nameserverManagementEditResult" data-form='nameserverManagementEdit'>
+			<i class="fa <?php if($POST->action=="add") { print "fa-plus"; } elseif ($POST->action=="delete") { print "fa-trash-o"; } else { print "fa-check"; } ?>"></i> <?php print $User->get_post_action(); ?>
 		</button>
 	</div>
 	<!-- result -->

app/admin/nat/edit-map.php
@@ -13,10 +13,8 @@ $Result 	= new Result ();
 # verify that user is logged in
 $User->check_user_session();
 
-# strip input tags
-$_POST = $Admin->strip_input_tags($_POST);
 # perm check popup
-if($_POST['action']=="edit") {
+if($POST->action=="edit") {
     $User->check_module_permissions ("nat", User::ACCESS_RW, true, true);
 }
 else {
@@ -24,13 +22,13 @@ else {
 }
 
 # validations
-if($_POST['object_type']!=="subnets" && $_POST['object_type']!=="ipaddresses")
+if($POST->object_type!=="subnets" && $POST->object_type!=="ipaddresses")
                                 { $Result->show("danger", _("Invalid type"), true, true); }
 
-$nat = $Admin->fetch_object("nat", "id", $_POST['id']);
+$nat = $Admin->fetch_object("nat", "id", $POST->id);
 if($nat===false)                { $Result->show("danger", _("Invalid Id"), true, true); }
 
-$object = $Admin->fetch_object($_POST['object_type'], "id", $_POST['object_id']);
+$object = $Admin->fetch_object($POST->object_type, "id", $POST->object_id);
 if($object===false)             { $Result->show("danger", _("Invalid object Id"), true, true); }
 
 
@@ -111,7 +109,7 @@ $icon =  $n->type=="static" ? "fa-arrows-h" : "fa-long-arrow-right";
 
     <h4><?php print _("New object"); ?>
     <?php
-    if($_POST['object_type']=="subnets")    { print $Tools->transform_address($object->subnet,"dotted")."/".$object->mask; }
+    if($POST->object_type=="subnets")    { print $Tools->transform_address($object->subnet,"dotted")."/".$object->mask; }
     else                                    { print $Tools->transform_address($object->ip_addr,"dotted"); }
     ?>
     </h4>
@@ -120,8 +118,8 @@ $icon =  $n->type=="static" ? "fa-arrows-h" : "fa-long-arrow-right";
     <?php print _("Add new object to NAT as"); ?>:
 	<div class="btns-group">
         <?php
-        print "<a class='btn btn-sm btn-success addNatObjectFromSearch' data-id='".escape_input($_POST['id'])."' data-object-id='$object->id' data-object-type='".escape_input($_POST['object_type'])."' data-type='src' data-reload='true'><i class='fa fa-plus'></i> "._('Source')."</a> ";
-        print "<a class='btn btn-sm btn-success addNatObjectFromSearch' data-id='".escape_input($_POST['id'])."' data-object-id='$object->id' data-object-type='".escape_input($_POST['object_type'])."' data-type='dst' data-reload='true'><i class='fa fa-plus'></i> "._('Destination')."</a>";
+        print "<a class='btn btn-sm btn-success addNatObjectFromSearch' data-id='".escape_input($POST->id)."' data-object-id='$object->id' data-object-type='".escape_input($POST->object_type)."' data-type='src' data-reload='true'><i class='fa fa-plus'></i> "._('Source')."</a> ";
+        print "<a class='btn btn-sm btn-success addNatObjectFromSearch' data-id='".escape_input($POST->id)."' data-object-id='$object->id' data-object-type='".escape_input($POST->object_type)."' data-type='dst' data-reload='true'><i class='fa fa-plus'></i> "._('Destination')."</a>";
         ?>
 	</div>
 

app/admin/nat/edit-result.php
@@ -16,7 +16,7 @@ $User->check_user_session();
 $User->check_maintaneance_mode ();
 
 # perm check popup
-if($_POST['action']=="edit") {
+if($POST->action=="edit") {
     $User->check_module_permissions ("nat", User::ACCESS_RW, true, false);
 }
 else {
@@ -26,37 +26,34 @@ else {
 # fetch custom fields
 $custom = $Tools->fetch_custom_fields('nat');
 
-# strip input tags
-$_POST = $Admin->strip_input_tags($_POST);
-
 # validate csrf cookie
-$User->Crypto->csrf_cookie ("validate", "nat", $_POST['csrf_cookie']) === false ? $Result->show("danger", _("Invalid CSRF cookie"), true) : "";
+$User->Crypto->csrf_cookie ("validate", "nat", $POST->csrf_cookie) === false ? $Result->show("danger", _("Invalid CSRF cookie"), true) : "";
 
 # validations
-if($_POST['action']=="delete" || $_POST['action']=="edit") {
-    if($Admin->fetch_object ('nat', "id", $_POST['id'])===false) {
+if($POST->action=="delete" || $POST->action=="edit") {
+    if($Admin->fetch_object ('nat', "id", $POST->id)===false) {
         $Result->show("danger",  _("Invalid NAT object identifier"), false);
     }
 }
-if($_POST['action']=="add" || $_POST['action']=="edit") {
+if($POST->action=="add" || $POST->action=="edit") {
     // name
-    if(strlen($_POST['name'])<3)                                            {  $Result->show("danger",  _("Name must have at least 3 characters"), true); }
-    if(!in_array($_POST['type'], array("source", "static", "destination"))) {  $Result->show("danger",  _("Invalid NAT type"), true); }
-    if(isset($_POST['device'])) {
-        if(!is_numeric($_POST['device']))                                   {  $Result->show("danger",  _("Invalid device"), true); }
+    if(strlen($POST->name)<3)                                            {  $Result->show("danger",  _("Name must have at least 3 characters"), true); }
+    if(!in_array($POST->type, array("source", "static", "destination"))) {  $Result->show("danger",  _("Invalid NAT type"), true); }
+    if(isset($POST->device)) {
+        if(!is_numeric($POST->device))                                   {  $Result->show("danger",  _("Invalid device"), true); }
     }
 }
 
 // set values
 // nothing to do here for l10n, the content of the array goes into the database
 $values = array(
-    "id"          => @$_POST['id'],
-    "name"        => $_POST['name'],
-    "type"        => $_POST['type'],
-    "src_port"    => $_POST['src_port'],
-    "dst_port"    => $_POST['dst_port'],
-    "device"      => $_POST['device'],
-    "description" => $_POST['description'],
+    "id"          => $POST->id,
+    "name"        => $POST->name,
+    "type"        => $POST->type,
+    "src_port"    => $POST->src_port,
+    "dst_port"    => $POST->dst_port,
+    "device"      => $POST->device,
+    "description" => $POST->description,
     "policy"      => "No",
     "policy_dst"  =>  ""
      );
@@ -66,9 +63,9 @@ if ($User->get_module_permissions ("devices")==User::ACCESS_NONE) {
 }
 
 // policy NAT override
-if($_POST['action']=="edit" && !is_blank($_POST['policy_dst'])) {
+if($POST->action=="edit" && !is_blank($POST->policy_dst)) {
     $values['policy']     = "Yes";
-    $values['policy_dst'] = $Tools->strip_input_tags($_POST['policy_dst']);
+    $values['policy_dst'] = $Tools->strip_input_tags($POST->policy_dst);
 }
 
 // append custom
@@ -76,18 +73,18 @@ if(sizeof($custom) > 0) {
     foreach($custom as $myField) {
         # replace possible ___ back to spaces!
         $myField['nameTest'] = str_replace(" ", "___", $myField['name']);
-        if(isset($_POST[$myField['nameTest']])) { $values[$myField['name']] = @$_POST[$myField['nameTest']];}
+        if(isset($POST->{$myField['nameTest']})) { $values[$myField['name']] = $POST->{$myField['nameTest']};}
     }
 }
 
 # execute update
-if(!$Admin->object_modify ("nat", $_POST['action'], "id", $values)) {
+if(!$Admin->object_modify ("nat", $POST->action, "id", $values)) {
     $Result->show("danger", _("NAT")." ".$User->get_post_action()." "._("failed"), false);
 }
 else {
     $Result->show("success", _("NAT")." ".$User->get_post_action()." "._("successful"), false);
 }
 # add
-if($_POST['action']=="add") {
+if($POST->action=="add") {
     print "<div class='new_nat_id hidden'>$Admin->lastId</div>";
 }

app/admin/nat/edit.php
@@ -17,7 +17,7 @@ $Result 	= new Result ();
 # verify that user is logged in
 $User->check_user_session();
 # perm check popup
-if($_POST['action']=="edit") {
+if($POST->action=="edit") {
     $User->check_module_permissions ("nat", User::ACCESS_RW, true, true);
 }
 else {
@@ -31,13 +31,13 @@ $csrf = $User->Crypto->csrf_cookie ("create", "nat");
 $Admin->validate_action();
 
 # get NAT object
-if($_POST['action']!="add") {
-	$nat = $Admin->fetch_object ("nat", "id", $_POST['id']);
+if($POST->action!="add") {
+	$nat = $Admin->fetch_object ("nat", "id", $POST->id);
 	$nat!==false ? : $Result->show("danger", _("Invalid ID"), true, true);
 }
 
 # disable edit on delete
-$readonly = $_POST['action']=="delete" ? "readonly" : "";
+$readonly = $POST->action=="delete" ? "readonly" : "";
 $link = $readonly ? false : true;
 
 # fetch custom fields
@@ -62,7 +62,7 @@ $custom = $Tools->fetch_custom_fields('nat');
             	<input type="text" class="form-control input-sm" name="name" value="<?php print $Tools->strip_xss($nat->name); ?>" placeholder='<?php print _('Name'); ?>' <?php print $readonly; ?>>
             	<input type="hidden" name="csrf_cookie" value="<?php print $csrf; ?>">
             	<input type="hidden" name="id" value="<?php print $nat->id; ?>">
-            	<input type="hidden" name="action" value="<?php print escape_input($_POST['action']); ?>">
+            	<input type="hidden" name="action" value="<?php print escape_input($POST->action); ?>">
         	</td>
         	<td>
             	<span class="text-muted"><?php print _("Select NAT name"); ?></span>
@@ -124,7 +124,7 @@ $custom = $Tools->fetch_custom_fields('nat');
         </tr>
 
     	<!-- Source -->
-    	<?php if($_POST['action']!=="add") { ?>
+    	<?php if($POST->action!=="add") { ?>
 
         <!-- Policy nat -->
         <tr class='port'>
@@ -175,7 +175,7 @@ $custom = $Tools->fetch_custom_fields('nat');
             	<span class="text-muted"></span>
         	</td>
         </tr>
-        <?php if($_POST['action']!=="delete") { ?>
+        <?php if($POST->action!=="delete") { ?>
     	<tr>
         	<th></th>
         	<td>
@@ -212,7 +212,7 @@ $custom = $Tools->fetch_custom_fields('nat');
             	<span class="text-muted"></span>
         	</td>
         </tr>
-        <?php if($_POST['action']!=="delete") { ?>
+        <?php if($POST->action!=="delete") { ?>
     	<tr>
         	<th></th>
         	<td>
@@ -288,7 +288,7 @@ $custom = $Tools->fetch_custom_fields('nat');
 <div class="pFooter">
 	<div class="btn-group">
 		<button class="btn btn-sm btn-default hidePopupsReload"><?php print _('Cancel'); ?></button>
-		<button class="btn btn-sm btn-default <?php if($_POST['action']=="delete") { print "btn-danger"; } else { print "btn-success"; } ?>" id="editNatSubmit"><i class="fa <?php if($_POST['action']=="add") { print "fa-plus"; } else if ($_POST['action']=="delete") { print "fa-trash-o"; } else { print "fa-check"; } ?>"></i> <?php print $User->get_post_action(); ?></button>
+		<button class="btn btn-sm btn-default <?php if($POST->action=="delete") { print "btn-danger"; } else { print "btn-success"; } ?>" id="editNatSubmit"><i class="fa <?php if($POST->action=="add") { print "fa-plus"; } elseif ($POST->action=="delete") { print "fa-trash-o"; } else { print "fa-check"; } ?>"></i> <?php print $User->get_post_action(); ?></button>
 	</div>
 	<!-- result -->
 	<div class="editNatResult"></div>

commit 20223ea10bae8c465cb89b86eefa7f7e84bb3fa4

Bugfix: Prepopulate the "name" field of the powerDNS "Record Edit" form, fixes #4363
Resolve a regression in behaviour introduced by d3d51b6e02285846cef979fcc5a2661626a76d2d
that caused the "Name" field of the powerDS "Record Edit" form to no longer be prepoluated
with the value in the POST variable "domain_id".

app/admin/powerDNS/record-edit.php
@@ -44,6 +44,10 @@ else {
 	// from IP table
 	// we provide record hostname and strip domain from it
 	if (!is_numeric($POST->domain_id) && !is_numeric($POST->id)) {
+		// Keep a copy of the textual $POST->domain_id, before it's dereferenced into
+		// a numeric idetifier, so that we can prepopulate the "Name" edit field.
+		$POST->name = $POST->domain_id;
+
 		// fetch all domains
 		$all_domains = $PowerDNS->fetch_all_domains ();
 		if ($all_domains!==false) {
@@ -82,6 +86,7 @@ else {
 		}
 		else {
 			$record = new Params ();
+			$record->name = $POST->name;
 			$record->ttl = (isset($pdns->ttl) && $pdns->ttl > 0) ? $pdns->ttl : 3600;
 			$record->content = $POST->id;
 		}