3d0f3bb7e62f92e3de05c089ad55a707075aa4a4
Blake Patton <bdot.02@gmail.com>
2026-03-17 11:29:08 -0700

    Fix bug where devices with zero height cause rack image generation to fail. (#4121)

--- functions/classes/class.Rackspace.php
+++ functions/classes/class.Rackspace.php
@@ -986,7 +986,7 @@ class RackContent extends Model {
      * @return int
      */
     public function getSize() {
-        return $this->size;
+        return max(1, $this->size);
     }
 
     /**
