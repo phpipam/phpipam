1fb1dd44d1dbd58bea2f0322b42e16f34831b857
GaryAllan <github@gallan.co.uk>
2026-03-21 21:53:33 +0000

    Update upgrade_queries_1.8.php

--- functions/upgrade_queries/upgrade_queries_1.8.php
+++ functions/upgrade_queries/upgrade_queries_1.8.php
@@ -12,7 +12,7 @@ $upgrade_queries["1.8.44"][] = "ALTER TABLE `lang` CHANGE `l_name` `l_name` VARC
 $upgrade_queries["1.8.44"][] = "-- Database version bump";
 $upgrade_queries["1.8.44"][] = "UPDATE `settings` SET `dbversion` = '44';";
 
-$upgrade_queries["1.8.45"]   = [];
-$upgrade_queries["1.8.45"][] = "ALTER TABLE `changelog` CHANGE `ctype` `ctype` SET('ip_addr','subnet','section','devices','vrf','vlans') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '';";
-$upgrade_queries["1.8.45"][] = "-- Database version bump";
-$upgrade_queries["1.8.45"][] = "UPDATE `settings` SET `dbversion` = '45';";
+$upgrade_queries["1.8.46"]   = [];
+$upgrade_queries["1.8.46"][] = "ALTER TABLE `changelog` CHANGE `ctype` `ctype` ENUM('ip_addr','subnet','section','devices','vrf','vlans') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;";
+$upgrade_queries["1.8.46"][] = "-- Database version bump";
+$upgrade_queries["1.8.46"][] = "UPDATE `settings` SET `dbversion` = '46';";
