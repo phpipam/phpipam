b73d498a8f2ed7d8d1cbbba78e9b641283499acb
Gary <github@gallan.co.uk>
2026-04-29 20:40:02 +0100

    Add \index.php
    phpIPAM v1.9.0 relocated all HTML application code from the project root directory into the \"public\" sub-directory.
    
    Please update your webserver, reverse-proxy or load-balancer configuration to point to the new location.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <title>phpIPAM installation</title>
+</head>
+
+<body>
+
+    <p>phpIPAM v1.9.0 relocated all HTML application code from the project root directory into the \"public\" sub-directory.</p>
+
+    <p>Please update your webserver, reverse-proxy or load-balancer configuration to point to the new location.</p>
+
+</body>
+
+</html>
